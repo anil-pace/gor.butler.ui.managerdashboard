fa1d25e692dfb4a0d4e48e93529566129a5be13c
Sourabh Jajoria <sourabh.j@GGN00329.local>
2016-08-23 17:34:37 +0530

    added tile and tile2x component added dependencies in package.json

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import React  from 'react';
 import ReactDOM  from 'react-dom';
+import Tilex from './shared.components/tile1x/Tilex';
+import Tile2x from './shared.components/tile1x/Tile2x';
+
+class App extends React.Component{
+	render(){
+		return (
+			<div>
+			<Tilex />
+			<Tilex />
+			<Tile2x />
+			</div>
+		);
+	}
+};
+ReactDOM.render(<App />,document.getElementById('container'));
