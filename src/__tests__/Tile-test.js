409c6ffa4f4048b4556ae86548ce473c8d0c1f41
Sourabh Jajoria <sourabh.j@GGN00329.local>
2016-08-26 11:24:22 +0530

    Testing in progress

--- src/__tests__/Tile-test.js
+++ src/__tests__/Tile-test.js
@@ -2,38 +2,38 @@
 
 
 // import React  from 'react';
-// import { shallow } from 'enzyme';
+// import { shallow, mount, render } from 'enzyme';
 // import Tilex from '../sharedComponents/tile1x/Tilex';
 
 // describe('Tilex  Tests', () => {
 	
 //     const wrapper = shallow(<Tilex />);
-//     console.log(wrapper)
+
 // 	it('Should contain 4 div elements',()=>{
-//   		expect(wrapper.find('.gorTile')).to.have.length(1);
+//   		expect(wrapper.find('div')).to.have.length(4);
 // 	});
 
-// 	it('Should contain 1 div with class "up-tile"',()=>{
-// 		expect(wrapper.find(div.gorUp-tile)).to.have.length(1);
-// 	});
+// 	// it('Should contain 1 div with class "up-tile"',()=>{
+// 	// 	expect(wrapper.find('div.gorUp-tile')).to.have.length(1);
+// 	// });
 
-// 	it('Should contain "Items in stock" in heading',()=>{
-// 		expect(wrapper.find('.gorHeading').text()).to.equal('Items in stock');
-// 	});
+// 	// it('Should contain "Items in stock" in heading',()=>{
+// 	// 	expect(wrapper.find('.gorHeading').text()).to.equal('Items in stock');
+// 	// });
 
-// 	it('Cart should be of type image',()=>{
-// 		expect(wrapper.find('cart').type()).to.equal('img');
+// 	// it('Cart should be of type image',()=>{
+// 	// 	expect(wrapper.find('cart').type()).to.equal('img');
 
-// 	});
+// 	// });
 
-// 	it('Expect 2 images',()=>{
-// 		expect(wrapper.find('img').length).to.equal(2);
+// 	// it('Expect 2 images',()=>{
+// 	// 	expect(wrapper.find('img').length).to.equal(2);
 
-// 	});
+// 	// });
 
-// 	it('Expect no buttons',()=>{
-// 		expect(wrapper.find('button').length).to.equal(0);
-// 	});
+// 	// it('Expect no buttons',()=>{
+// 	// 	expect(wrapper.find('button').length).to.equal(0);
+// 	// });
 
 // });
 
