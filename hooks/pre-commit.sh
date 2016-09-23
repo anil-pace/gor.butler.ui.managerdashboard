2b8a7854342bf4fea628cb66c4c92eedc73f98c8
Himanshu Saraswat <himanshu.s@GGN00338.local>
2016-09-23 23:56:18 +0530

    adding eslint rules in the config file

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -3,11 +3,16 @@
 function lintit () {
   OUTPUT=$(git diff HEAD --name-only | grep -E '(.js)$')
   a=("${(f)OUTPUT}")
+  if [[ "$a" != "" ]];then
   e=$(node_modules/.bin/eslint $a)
   echo $e
   if [[ "$e" != *"0 problems"* ]]; then
     echo "ERROR: Check eslint hints."
     exit 1 # reject
   fi
+else
+	echo "No JS file modified"
+	exit 0
+fi
 }
 lintit
