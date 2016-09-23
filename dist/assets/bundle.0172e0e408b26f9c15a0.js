9be114b4609d6a8396678d3cf9a39e2d62803f1b
arshdeep <arshdeep.m@greyorange.sg>
2016-09-23 12:54:30 +0530

    - fixing webpackJsonp issue in production build

---
+++ dist/assets/bundle.0172e0e408b26f9c15a0.js
@@ -0,0 +1,16 @@
+webpackJsonp([4],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	__webpack_require__(20);
+	__webpack_require__(53);
+	__webpack_require__(191);
+	__webpack_require__(215);
+	__webpack_require__(198);
+	__webpack_require__(461);
+	__webpack_require__(462);
+	module.exports = __webpack_require__(463);
+
+
+/***/ }
+]);
