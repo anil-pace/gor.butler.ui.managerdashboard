0535c71f65f68cb0ff7a8be15de89aa6db2abcc5
Himanshu Saraswat <himanshu.s@GGN00338.local>
2016-09-27 12:37:09 +0530

    changing the IP to localhost

--- src/constants/configConstants.js
+++ src/constants/configConstants.js
@@ -1,2 +1,2 @@
-export const WS_URL = "wss://192.168.12.168/manager_api/wss";
-export const LOGIN_URL="https://192.168.12.168/api/auth/token"
+export const WS_URL = "wss://localhost/manager_api/wss";
+export const LOGIN_URL="https://localhost/api/auth/token"
