3756600c124e2a67da8d3ff0711a4d89226f4cde
Sourabh Jajoria <sourabh.j@GGN00329.local>
2016-10-19 22:35:37 +0530

    Datalist component started

---
+++ src/components/datalist/datalist.js
@@ -0,0 +1,25 @@
+import React  from 'react';
+import ReactDOM  from 'react-dom';
+
+class DataList extends React.Component{
+	constructor(props) 
+	{
+    	super(props);
+    }
+	render(){
+        var opt=[{v:1},{v:2},{v:3},{v:4}],temp='';
+        for(let i=0;i<opt.length;i++)
+        {
+            temp+='<option value='+opt[i].v+' >';
+        }
+		return (
+		<div>
+            <input type="text" list="droplist" />
+            <datalist id="droplist">
+            {temp}
+            </datalist>
+		</div> 
+		);
+	}
+};
+export default DataList;
