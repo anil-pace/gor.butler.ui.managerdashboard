ef806ecc06810e0cb3678dd7a0071e1ed7011fbc
Sourabh Jajoria <sourabh.j@GGN00329.local>
2016-11-07 10:47:32 +0530

    Bug fixes

--- src/constants/configConstants.js
+++ src/constants/configConstants.js
@@ -1,11 +1,11 @@
 
-export const BASE_URL = '192.168.8.118';
+export const BASE_URL = '192.168.8.120';
 export const PORT = '8888';
 export const PROTOCOL = 'https://';
 export const WS_PROTOCOL ="wss://";
 export const WS_URL = WS_PROTOCOL+BASE_URL+"/manager_api/wss"
 export const LOGIN_URL=PROTOCOL+BASE_URL+"/api/auth/token"
-export const API_URL=PROTOCOL+BASE_URL+"/api/"
+export const API_URL=PROTOCOL+BASE_URL+"/api"
 export const HEADER_URL = PROTOCOL+BASE_URL+"/api/user"
 export const ORDERS_URL="/orders/md"
 export const PPS_MODE_CHANGE_URL = "pps/";
