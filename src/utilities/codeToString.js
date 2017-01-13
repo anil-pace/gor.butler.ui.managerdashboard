1aed66c7fb6c3caf4b22afd05b9c66f4a42a3b83
Sourabh Jajoria <sourabh.j@greyorange.sg>
2017-01-13 12:53:27 +0530

    Changes in status for orders tile and audit delete notification

--- src/utilities/codeToString.js
+++ src/utilities/codeToString.js
@@ -32,7 +32,7 @@ export function codeToString(res){
               stringInfo={
                 type:SUCCESS,
                 msg:(<FormattedMessage id="notify.delete.audit.success" description='Text for successfull audit deletion' 
-            defaultMessage="Audit task {audit_id} has been deleted" values={{audit_id:res.details[1]||"--"}}/>)
+            defaultMessage="Audit task {audit_id} has been deleted" values={{audit_id:res.details["display_id"]||"--"}}/>)
               }
               break;              
             case CODE_UE001:
