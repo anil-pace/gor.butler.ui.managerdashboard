acdcf70898b0a342876ed7e3d3a1602e69fd1cbb
himanshu.s <himanshu.s@greyorange.sg>
2017-04-12 15:17:01 +0530

    changed the URLs to relative

--- src/constants/configConstants.js
+++ src/constants/configConstants.js
@@ -3,16 +3,16 @@ export const BASE_URL = '192.168.8.155' ;
 export const PORT = '8888';
 export const PROTOCOL = 'https://';
 export const WS_PROTOCOL ="wss://";
-export const WS_URL = WS_PROTOCOL+BASE_URL+"/manager_api/wss"
-export const LOGIN_URL=PROTOCOL+BASE_URL+"/api/auth/token"
-export const API_URL=PROTOCOL+BASE_URL+"/api"
-export const HEADER_URL = PROTOCOL+BASE_URL+"/api/user"
+export const WS_URL = "/manager_api/wss"
+export const LOGIN_URL="/api/auth/token"
+export const API_URL="/api"
+export const HEADER_URL = "/api/user"
 export const ORDERS_URL="/orders/md"
 export const PPS_MODE_CHANGE_URL = "/pps/";
 export const PAGE_SIZE_URL = "&PAGE_SIZE=";
 export const ROLE_URL=HEADER_URL+'/role';
 export const CHECK_USER=HEADER_URL+'?username=';
-export const AUDIT_URL = PROTOCOL+BASE_URL+"/api/audit";
+export const AUDIT_URL = "/api/audit";
 export const DELETE_AUDIT_URL=AUDIT_URL+'/delete/';
 export const START_AUDIT_URL=AUDIT_URL+'/start';
 export const SEARCH_AUDIT_URL=AUDIT_URL + "/search?start_time=";
@@ -24,7 +24,7 @@ export const UPDATE_TIME_LOW='&update_time<=';
 export const UPDATE_TIME_HIGH='&update_time>=';
 export const PICK_BEFORE_ORDER_URL = "&pick_before_time<=";
 export const BREACHED_URL = "&breached=true&warehouse_status=['pending','fulfillable','temporary_unfulfillable','completed']";
-export const TIME_ZONE_URL = PROTOCOL+BASE_URL+"/api/components/get_time_zone"
+export const TIME_ZONE_URL = "/api/components/get_time_zone"
 export const GIVEN_PAGE="&page=";
 export const GIVEN_PAGE_SIZE="&PAGE_SIZE=20";
 export const FILTER_ORDER_ID =  "&order_id~=";
