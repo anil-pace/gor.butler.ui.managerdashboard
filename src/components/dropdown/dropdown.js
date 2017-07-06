002b9fa7246947e9e7f2367c9fd1f4991df41c39
arshdeep <arshdeep.m@greyorange.sg>
2017-07-06 18:10:23 +0530

    Removed the time out on the dropdown https://work.greyorange.com/jira/browse/BSS-3902

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -5,42 +5,35 @@ import Ddown from 'react-dropdown';
 
 
 class Dropdown extends Component {
-  constructor (props) {
-    super(props)
-    this.state={selected: { value: props.currentState.value, label: props.currentState.label}
-    }
-    this._onSelect=this._onSelect.bind(this)
-  }
+	constructor (props) {
+		super(props)
+		this.state={selected: { value: props.currentState.value, label: props.currentState.label}
+		}
+		this._onSelect=this._onSelect.bind(this)
+	}
 
+	_onSelect (option) {
+		this.setState({selected: option})
+		this.props.optionDispatch(option.value);
+		var _this=this;
+	}
 
-  _onSelect (option) {
-    this.setState({selected: option})
-    this.props.optionDispatch(option.value);
-    var _this=this;
-    setTimeout(function(){
-      if(_this.props.refreshList) {
-      _this.props.refreshList();
-    }
-    },0)
-    
-  }
+	shouldComponentUpdate(nextProps,nextState){
+		if (this.state.selected.value===nextState.selected.value)
+		{return false;}
+		else 
+		{return true;}
+	}
 
-  shouldComponentUpdate(nextProps,nextState){
-    if (this.state.selected.value===nextState.selected.value)
-      {return false;}
-    else 
-      {return true;}
-  }
+	render () {
+		const defaultOption=this.state.selected
 
-  render () {
-    const defaultOption=this.state.selected
-
-    return (
-      <div className={this.props.styleClass}>
-        <Ddown options={this.props.items} onChange={this._onSelect} value={defaultOption}  />
-      </div>
-    )
-  }
+		return (
+			<div className={this.props.styleClass}>
+			<Ddown options={this.props.items} onChange={this._onSelect} value={defaultOption}  />
+			</div>
+		)
+	}
 }
 
 export default Dropdown;
