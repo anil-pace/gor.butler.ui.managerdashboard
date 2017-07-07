b926acaafd3ba51fed66d578deed51fda0e50682
Gaurav Makkar <gaurav.m@Gaurav-MacBook-Pro.local>
2017-07-07 15:02:49 +0530

    Fix for BSS-3992 https://work.greyorange.com/jira/browse/BSS-3992

--- src/components/commonFunctionsDataTable.js
+++ src/components/commonFunctionsDataTable.js
@@ -364,8 +364,8 @@ export const AuditIssuesTooltipCell = ({rowIndex, data, columnKey, setClass, cal
 
 
 
-            <div  className="gor-tool-tip-hover" style={{fontSize:16,color:'black'}} onMouseEnter={callBack}>
-                {data.getObjectAt(rowIndex)[columnKey]} <span className="gor-audit-info-icon"/>
+            <div  className="gor-tool-tip-hover" style={{fontSize:16,color:'black'}}>
+                {data.getObjectAt(rowIndex)[columnKey]} <span className="gor-audit-info-icon" onMouseEnter={callBack}/>
             </div>:data.getObjectAt(rowIndex)[columnKey]
 
 
