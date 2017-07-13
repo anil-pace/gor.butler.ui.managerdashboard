0decafdddf8823097471031fa6cef8f5df67a80c
himanshu.s <himanshu.s@greyorange.sg>
2017-07-13 19:30:05 +0530

    BSS-3859: https://work.greyorange.com/jira/browse/BSS-3859 BSS-3989 : https://work.greyorange.com/jira/browse/BSS-3989 BSS-3807https://work.greyorange.com/jira/browse/BSS-3807

--- src/components/gor-dropdown-component/list.js
+++ src/components/gor-dropdown-component/list.js
@@ -5,14 +5,23 @@ class List extends Component {
     super(props);
   }
   _processOptions(){
-    var optionList=[], data = this.props.data,
+    var optionList=[], data = this.props.data || [],
     optionsLen=data.length;
     for(let index=0;index<optionsLen;index++){
-      optionList.push(<span className={data[index].disabled?"disabled gor-dropdown-option" : "gor-dropdown-option"}  key={index} onClick={!data[index].disabled ? this.props.optionAction.bind(null,index) : null} >
+      
+      optionList.push(<span className={data[index].disabled?"disabled gor-dropdown-option" : "gor-dropdown-option"}  key={index} onClick={!data[index].disabled ? this.props.optionAction.bind(null,data[index].value,null) : null} >
                         <span >{data[index].label}</span>
                       </span>);
     }
     return optionList;
+  }
+componentWillReceiveProps(nextProps) {
+     if(nextProps.data.length !== this.props.data.length)  {
+      this.props.optionAction.call(null,this.props.selectedOption,true);
+     }
+ }
+  componentDidMount(){
+    this.props.data && this.props.selectedOption ? this.props.optionAction.call(null,this.props.selectedOption,true) : "";
   }
   render() {
     var optionList=this._processOptions();
