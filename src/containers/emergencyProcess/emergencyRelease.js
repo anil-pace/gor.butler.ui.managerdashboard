4d0dda332748523f8b32057f5a0feae6d2576159
Raja D <raja.d@greyorange.sg>
2017-07-21 16:09:28 +0530

    changed IP config https://work.greyorange.com/jira/browse/BSS-4002

--- src/containers/emergencyProcess/emergencyRelease.js
+++ src/containers/emergencyProcess/emergencyRelease.js
@@ -27,7 +27,7 @@ class EmergencyRelease extends React.Component{
       size: 'large', // large, medium or small,
       closeOnOutsideClick: false, // (optional) Switch to true if you want to close the modal by clicking outside of it,
       hideCloseButton: true, // (optional) if you don't wanna show the top right close button
-      EmergencyReleased :true
+      emergencyReleased :true
       //.. all what you put in here you will get access in the modal props ;)
     });
   }
