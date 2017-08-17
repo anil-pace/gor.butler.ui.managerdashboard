650d3831728e86c13b35daafeb00534863fe80f6
himanshu.s <himanshu.s@greyorange.sg>
2017-08-17 14:10:33 +0530

    Smart component test case added for zoning

--- src/__tests__/components/zoning/sysOverview-container-tests.js
+++ src/__tests__/components/zoning/sysOverview-container-tests.js
@@ -1,25 +1,85 @@
 import React from 'react';
-import {SystemOverview} from '../../../components/systemOverview/zoneTile.js';
-import { shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import SystemOverview from '../../../containers/systemTabs/sysOverview.js';
+import Zone from '../../../components/systemOverview/zoneTile.js';
+import {messages} from '../../../../mock/mock-intl-json.js';
+import {IntlProvider} from 'react-intl-redux';
+import { shallow ,mount} from 'enzyme';
 import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store'
 
-describe('REACT-REDUX (Shallow + passing the {store} directly)',()=>{
-    const initialState = {subscribed:false}
+const intl_messages = messages
+
+ 
+describe('REACT-REDUX (Deep + passing the {store} using provider)',()=>{
+    const initialState = {
+      subscribed:false,
+      recieveSocketActions:{
+        socketAuthorized:true
+      },
+      sysOverviewReducer:{
+        hasDataChanged:true,
+        zones:{
+          "zones_data":{
+                "1": {
+                  "zone_status": "operation_normal" 
+                },
+                "2": {
+                  "zone_status": "emergency_pause"
+                },
+                "3":{
+                  "zone_status": "emergency_stop"
+                },
+                "4":{
+                  "zone_status": "zone_pause_activated"
+                },
+                "5":{
+                  "zone_status": "zone_clear_initiated"
+                },
+                "6":{
+                  "zone_status": "zone_pause_initiated"
+                },
+                "7":{
+                  "zone_status": "zone_clear_activated"
+                },
+                "8":{
+                  "zone_status": "zone_clear_deactivated"
+                }
+              },
+          "emergency_data":{
+            "emergency_on": false,
+            "emergency_type":"stop"
+          }
+        },
+      zoneSubscriptionInitiated:true
+      
+    },
+    zoningReducer:{
+          zoneHeader:{}
+    },
+    intl:{
+      locale:"en-US"
+    }
+  }
     const mockStore = configureStore()
     let store,container
 
     beforeEach(()=>{
         store = mockStore(initialState)
-        container = shallow(<SystemOverview store={store} /> )  
+        container = mount( <Provider store={store}>
+        <IntlProvider messages={intl_messages} locale={"en-US"} >
+        <SystemOverview />
+        </IntlProvider>
+        </Provider> )//shallow(<SystemOverview store={store} /> )  
     })
 
     it('+++ render the connected(SMART) component', () => {
-       expect(container.length).toEqual(1)
+       expect(container.find(SystemOverview).length).toEqual(1)
     });
-
-   /* it('+++ check Prop matches with initialState', () => {
-       expect(container.prop('output')).toEqual(initialState.subscribed)
-    });*/
+    
+    it('+++ Checking count of zone tiles', () => {
+       expect(container.find(Zone).length).toEqual(8)
+    });
+    
 
 });
