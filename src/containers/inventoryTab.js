80b72da418fd30fbec64ac8bb88be5aea416bb02
himanshu.s <himanshu.s@greyorange.sg>
2017-10-26 00:05:21 +0530

    https://work.greyorange.com/jira/browse/BSS-4269

--- src/containers/inventoryTab.js
+++ src/containers/inventoryTab.js
@@ -15,10 +15,10 @@ import {WS_ONSEND} from './../constants/frontEndConstants';
 
 
 class InventoryTab extends React.Component{
-	constructor(props) 
-	{
-    	super(props);
-    	this.state={subscribed:false}
+    constructor(props) 
+    {
+        super(props);
+        this.state={subscribed:false}
     }
 
     componentWillMount() {
@@ -32,8 +32,8 @@ class InventoryTab extends React.Component{
     }
 
     _setSpinner(bShow) {
-    	var _bShow=bShow ? bShow:false;
-    	this.props.setInventorySpinner(_bShow);
+        var _bShow=bShow ? bShow:false;
+        this.props.setInventorySpinner(_bShow);
     }
 
     componentWillReceiveProps(nextProps) {
@@ -46,54 +46,52 @@ class InventoryTab extends React.Component{
     }
     componentWillUnmount(){
         /**
-		 * If a user navigates back to the inventory page,
-		 * it should subscribe to the packet again.
-		 */
-		this.setState({subscribed: false})
-	}
+         * If a user navigates back to the inventory page,
+         * it should subscribe to the packet again.
+         */
+        this.setState({subscribed: false})
+    }
 
     _subscribeData(nextProps){
         let updatedWsSubscription=this.props.wsSubscriptionData;
         this.props.initDataSentCall(updatedWsSubscription["inventory"])
         this.props.updateSubscriptionPacket(updatedWsSubscription);
-	}
+    }
 
-	render(){
-		/**
-		 * Need to remove these hardcoded variables
-		 * 
-		 */
-		var histogramLabel=<FormattedMessage id="inventory.histogram.header" description="Inventory Histogram Header Message" 
-              			defaultMessage="Stock level history"/>  ,
-		linechartLabel=<FormattedMessage id="inventory.linechart.header" description="Inventory Line Chart Header Message" 
-              			defaultMessage="Item Movements"/>,
+    render(){
+        
+        var histogramLabel=<FormattedMessage id="inventory.histogram.header" description="Inventory Histogram Header Message" 
+                        defaultMessage="Stock level history"/>  ,
+        linechartLabel=<FormattedMessage id="inventory.linechart.header" description="Inventory Line Chart Header Message" 
+                        defaultMessage="Item Movements"/>,
         dateTodayState=this.props.dateTodayState,
-		snapshotData=this.props.recreatedData[dateTodayState] ? this.props.recreatedData[dateTodayState].otherInfo: {}
-		return (
-			<div className="gorInventory wrapper">
-				<Spinner isLoading={this.props.inventorySpinner} setSpinner={this.props.setInventorySpinner}/>
-				<Inventory noData={this.props.noData} 
-				recreatedData={this.props.recreatedData} 
-				currentDate={this.props.dateTodayState} 
-				hasDataChanged={this.props.hasDataChanged} 
-				histogramLabel={histogramLabel} 
-				linechartLabel={linechartLabel} 
-				isPrevDateSelected={this.props.isPrevDateSelected} 
-				inventoryDataPrevious={this.props.inventoryDataPrevious} 
-				snapshotData={snapshotData}/>
-			</div>
-		);
-	}
+        snapshotData=this.props.recreatedData[dateTodayState] ? this.props.recreatedData[dateTodayState].otherInfo: {}
+        return (
+            <div className="gorInventory wrapper">
+                <Spinner isLoading={this.props.inventorySpinner} setSpinner={this.props.setInventorySpinner}/>
+                <Inventory noData={this.props.noData} 
+                recreatedData={this.props.recreatedData} 
+                currentDate={this.props.dateTodayState} 
+                hasDataChanged={this.props.hasDataChanged} 
+                histogramLabel={histogramLabel} 
+                linechartLabel={linechartLabel} 
+                isPrevDateSelected={this.props.isPrevDateSelected} 
+                inventoryDataPrevious={this.props.inventoryDataPrevious} 
+                snapshotData={snapshotData}
+                timeOffset={this.props.timeOffset}/>
+            </div>
+        );
+    }
 };
 
 InventoryTab.propTypes={
-	inventorySpinner:React.PropTypes.bool,
-	isPrevDateSelected:React.PropTypes.bool,
-	inventoryDataPrevious:React.PropTypes.object ,
-	hasDataChanged:React.PropTypes.bool,
-	dateTodayState:React.PropTypes.number,
-	recreatedData: React.PropTypes.object,
-	noData:React.PropTypes.bool
+    inventorySpinner:React.PropTypes.bool,
+    isPrevDateSelected:React.PropTypes.bool,
+    inventoryDataPrevious:React.PropTypes.object ,
+    hasDataChanged:React.PropTypes.bool,
+    dateTodayState:React.PropTypes.number,
+    recreatedData: React.PropTypes.object,
+    noData:React.PropTypes.bool
 }
 
 function mapStateToProps(state, ownProps) {
@@ -107,23 +105,23 @@ function mapStateToProps(state, ownProps) {
         "recreatedData": state.inventoryInfo.recreatedData || {},
         "noData": state.inventoryInfo.noData,
         wsSubscriptionData: state.recieveSocketActions.socketDataSubscriptionPacket || wsOverviewData,
-        socketAuthorized: state.recieveSocketActions.socketAuthorized
+        socketAuthorized: state.recieveSocketActions.socketAuthorized,
+        timeOffset:state.authLogin.timeOffset
     }
 }
     function mapDispatchToProps(dispatch){
-    	return{
-    		setInventorySpinner:function(data){dispatch(setInventorySpinner(data));},
+        return{
+            setInventorySpinner:function(data){dispatch(setInventorySpinner(data));},
             initDataSentCall: function(data){ dispatch(setWsAction({type:WS_ONSEND,data:data})); },
             updateSubscriptionPacket: function (data) {
                 dispatch(updateSubscriptionPacket(data));
             },
             inventoryRefreshed:function(data){
-            	dispatch(inventoryRefreshed(data))
-			}
-    	}
+                dispatch(inventoryRefreshed(data))
+            }
+        }
     };
 
 
 export default connect(mapStateToProps,mapDispatchToProps)(InventoryTab);
-
 
