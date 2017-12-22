9fe104d396f798577382cca65044dc53b90db51a
Pranjal Deshmukh <pranjal.d@greyorange.sg>
2017-12-22 16:37:06 +0530

    https://work.greyorange.com/jira/browse/BSS-5651

---
+++ src/components/InputComponent/InputComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const InputComponent = (props) => {
+  return (
+    
+    <input className={props.className} onChange={props.updateInput.bind(this,props.index)} type="text" value= {props.value}/>
+    
+    );
+  
+
+}
