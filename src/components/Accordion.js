4b945c7f1914fb3710774875967f0fe2d3bc82d1
Anil Kumar <anil.k@GGN000046.local>
2018-01-28 03:14:25 +0530

    accordion in progress

---
+++ src/components/Accordion.js
@@ -0,0 +1,50 @@
+//source: https://codepen.io/adamaoc/pen/wBGGQv?editors=1010
+
+import React  from 'react';
+import { FormattedMessage } from 'react-intl';
+
+class Accordion extends React.Component{
+  constructor(props) 
+  {
+      super(props); 
+      this.handleClick = this.handleClick.bind(this);
+      this.state={
+        open: false,
+        class: "panel"
+      }
+      
+  }
+
+  handleClick(){
+    if(this.state.open) {
+      this.setState({
+        open: false,
+        class: "panel"
+      });
+    }else{
+      this.setState({
+        open: true,
+        class: "panel open"
+      });
+    }
+  }
+  
+  render()
+  {
+      return (
+        <div className="main">
+          <div className={this.state.class}>
+            <div className="panelHeader" onClick={this.handleClick}>{this.props.title}</div>
+            <div className="panelWrapper">
+              <div className="panelContent">
+                {(this.state.class === "panel open") ? this.props.children : null}
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  export default Accordion;
+
--- src/containers/newordersTab.js
+++ src/containers/newordersTab.js
@@ -4,23 +4,19 @@
  */
 import React  from 'react';
 //import OrdersSubTab from '../components/subtab/ordersTabs';
-import Tilex from '../components/tile2x/Tile2x';
-//import Accordion from '../components/Accordion';
 import {GTable} from '../components/gor-table-component/index'
 import {GTableHeader,GTableHeaderCell} from '../components/gor-table-component/tableHeader';
 import {GTableBody} from "../components/gor-table-component/tableBody";
 import {GTableRow} from "../components/gor-table-component/tableRow";
 import {FormattedMessage} from 'react-intl';
 import ProgressBar from '../components/ProgressBar.js';
+import Accordion from '../components/Accordion.js';
 
-var items ={
-	"pageId": "orders",
-	"headingleft": "Order fulfuilment progress",
-	"textleft": "No PPS running",
-	"statusleft": "No products to be picked",
-	"headingright": "Order summary",
-
-
+var orderStrings ={
+	"ordersCompleted": "Order(s) completed of",
+	"orderLineCompleted": "Orderline(s) completed of",
+	"breachedOrders": "Breached orders",
+	"inProgress": "currently in progress"
 }
 
 class newordersTab extends React.Component{
@@ -55,90 +51,114 @@ class newordersTab extends React.Component{
 	render(){
 		console.log("coming inside new orders Tab");
 		var processedData=[
-							{id:1, text: "SKU CODE", sortable: true}, 
-							{id:2, text: "NAME", sortable: true},
-							{id:3, text: "OPENING STOCK", searchable: false}, 
-							{id:4, text: "PURCHAGE QTY", searchable: false}, 
-							{id:5, text: "SALE QTY", sortable: true}, 
-							{id:6, text: "CLOSING STOCK", sortable: true}
-						]
+							{id: "Wave ID 001", text: "SKU CODE", sortable: true}, 
+							{id: "Wave ID 002", text: "NAME", sortable: true},
+							{id: "Wave ID 003", text: "OPENING STOCK", searchable: false}, 
+							{id: "Wave ID 004", text: "PURCHAGE QTY", searchable: false}, 
+							{id: "Wave ID 005", text: "SALE QTY", sortable: true}, 
+							{id: "Wave ID 006", text: "CLOSING STOCK", sortable: true}
+						];
+
+	   var title = (
+                    <GTableRow key={1} index={1} offset={processedData} max={processedData} data={processedData}>
+                        <div key={1} style={processedData[0].width?{flex:'1 0 '+processedData[0].width+"%"}:{}} className="cell" > WAVE ROW 1
+                            
+                        </div>
+                    </GTableRow>
+			                        
+	            );
 		return(
 			<div>
-			  <div className="orderTopWrapper">
-
-			  	<div className="orderLeftWrapper">
-			  		<div className="orderLeftContent">
-				  		<div className="dateTimeWrapper">
-				  			<span className="dateTime"> {this.state.todaysDate} </span>
-				  		</div>
-				  		<div className="orderLeftHeader"> 
-				  			<FormattedMessage id="orders.progress" description="header of orders progress" defaultMessage="Order fulfuilment progress "/>
-				  		</div>
-				  		<ProgressBar progressWidth={this.state.progressCount}/>
-				  		<div className="orderLeftStatus">
-				  			<div className="statusLeft">
-				  				<span> No PPS running </span>
-				  			</div>
-				  			<div className="statusRight">
-				  				<span> No Products to be picked </span>
-				  			</div>
-				  		</div>
+			  	<div className="orderTopWrapper">
+				  	<div className="orderLeftWrapper">
+				  		<div className="orderLeftContent">
+					  		<div className="dateTimeWrapper">
+					  			<span className="dateTime"> {this.state.todaysDate} </span>
+					  		</div>
+					  		<div className="orderLeftHeader"> 
+					  			<FormattedMessage id="orders.progress" description="header of orders progress" defaultMessage="Order fulfuilment progress "/>
+					  		</div>
+					  		<ProgressBar progressWidth={this.state.progressCount}/>
+					  		<div className="orderLeftStatus">
+					  			<div className="statusLeft">
+					  				<span> No PPS running </span>
+					  			</div>
+					  			<div className="statusRight">
+					  				<span> No Products to be picked </span>
+					  			</div>
+					  		</div>
+					  	</div>
 				  	</div>
-			  	</div>
 
-			  	<div className="orderCenterWrapper"> </div>
+				  	<div className="orderCenterWrapper"> </div>
 
-			  	<div className="orderRightWrapper">
-			  		<div className="orderRightContent">
-				  		<div className="orderButtonWrapper">
-				  			<div className="gorButtonWrap">
-							    <button className="ordersCollapseAll">
-							    <FormattedMessage id="orders.table.collapseAll" description="button label for collapse all" defaultMessage="COLLAPSE ALL "/>
-							    </button>
-						    </div>
-							<div className="gorButtonWrap">
-							    <button className="ordersFilterData">
-							    <div className="gor-manage-task"/>
-							    <FormattedMessage id="orders.tabel.filterLabel" description="button label for filter" defaultMessage="FILTER DATA"/>
-							    </button>
-							</div>
-				  		</div>
+				  	<div className="orderRightWrapper">
+				  		<div className="orderRightContent">
+					  		<div className="orderButtonWrapper">
+					  			<div className="gorButtonWrap">
+								    <button className="ordersCollapseAll">
+								    <FormattedMessage id="orders.table.collapseAll" description="button label for collapse all" defaultMessage="COLLAPSE ALL "/>
+								    </button>
+							    </div>
+								<div className="gorButtonWrap">
+								    <button className="ordersFilterData">
+								    <div className="gor-manage-task"/>
+								    <FormattedMessage id="orders.tabel.filterLabel" description="button label for filter" defaultMessage="FILTER DATA"/>
+								    </button>
+								</div>
+					  		</div>
 
-				  		<div className="orderRightHeader"> 
-				  			<FormattedMessage id="orders.summary" description="header of orders summary" defaultMessage="Order summary "/>
-				  		</div>
-				  		<div className="orderRightStatus">
-				  			<div className="statusTop">
-				  				<span> 0 Order(s) completed of 0 </span>
-				  				<span> 0 Orderline(s) completed of 0 </span>
-				  				<span> 0 Breached orders </span>
-				  			</div>
-				  			<div className="statusBottom">
-				  				<span> 0 currently in progress </span>
-				  				<span> 0 currently in progress </span>
-				  				<span> 0 Breached orders completed</span>
-				  			</div>
-				  		</div>
+					  		<div className="orderRightHeader"> 
+					  			<FormattedMessage id="orders.summary" description="header of orders summary" defaultMessage="Order summary "/>
+					  		</div>
+					  		<div className="orderRightStatus">
+					  			<div className="statusTop">
+					  				<span> 0 {orderStrings.ordersCompleted} 0 </span>
+					  				<span> 0 {orderStrings.orderLineCompleted} 0 </span>
+					  				<span> 0 {orderStrings.breachedOrders} </span>
+					  			</div>
+					  			<div className="statusBottom">
+					  				<span> 0 {orderStrings.inProgress} </span>
+					  				<span> 0 {orderStrings.inProgress}</span>
+					  				<span> 0 {orderStrings.breachedOrders} completed</span>
+					  			</div>
+					  		</div>
+					  	</div>
 				  	</div>
-			  	</div>
+			    </div> 
 
-			  </div>
-			  <GTable options={['table-bordered']}>
-	                
-	                <GTableBody data={processedData} >
-	                    {processedData ? processedData.map(function (row, idx) {
-	                        return (
-		                            <GTableRow key={idx} index={idx} offset={processedData} max={processedData} data={processedData}>
-		                                {Object.keys(row).map(function (text, index) {
-		                                    return <div key={index} style={processedData[index].width?{flex:'1 0 '+processedData[index].width+"%"}:{}} className="cell" >
-		                                        {text}
-		                                    </div>
-		                                })}
-		                            </GTableRow>
-	                        )
-	                    }):""}
-	                </GTableBody>
-	            </GTable>
+			    <div className="waveListWrapper">
+				    <GTable options={['table-bordered']}>
+		                <GTableBody data={processedData} >
+		                    {processedData ? processedData.map(function (row, idx) {
+		                        return (
+		                            <Accordion title="Wave Row 1">
+		                            	<GTable options={['table-bordered']}>
+							                <GTableBody data={processedData} >
+							                    {processedData ? processedData.map(function (row, idx) {
+							                        return (
+							                            <GTableRow key={idx} index={idx} offset={processedData} max={processedData} data={processedData}>
+							                                {Object.keys(row).map(function (text, index) {
+							                                    return <div key={index} style={processedData[index].width?{flex:'1 0 '+processedData[index].width+"%"}:{}} className="cell" >
+							                                        {text}
+							                                    </div>
+							                                })}
+							                            </GTableRow>
+							                        )
+							                    }):""}
+							                </GTableBody>
+					            		</GTable>
+					            	</Accordion>
+
+		                        )
+		                    }):""}
+		                </GTableBody>
+            		</GTable>
+			        <Accordion title="Wave Row 2">   
+			            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nemo harum voluptas aliquid rem possimus nostrum excepturi!
+			            
+			        </Accordion>
+			  	</div>
 			</div>
 			)
 	}
