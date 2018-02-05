96b3f4ab1489660ff82ad680468138af444e8bf3
Raja D <raja.d@greyorange.sg>
2018-02-05 14:13:08 +0530

    Initial name Component

--- src/components/NameInitial/nameInitial.js
+++ src/components/NameInitial/nameInitial.js
@@ -11,17 +11,21 @@ class NameInitial extends React.Component{
 
 render()
 {
+    let type=this.props.type;
     let originalName=this.props.name;
-    let type=this.props.shape?this.props.shape:'round';
+    let shape=this.props.shape?this.props.shape:'round';
     let name=originalName.split(' ');
     let firStNameInitial=name[0]?name[0].charAt(0):'';
     let lastnameInitial=name[1]?name[1].charAt(0):'';
 
     return (
        <div className="gor-IN-wrapper">
-           <div className={"gor-IN-"+type+""} title={originalName}>
+       {!type?
+           <div className={"gor-IN-"+shape+""} title={originalName}>
            <span className="got-IN-initial">{firStNameInitial.toUpperCase()}{lastnameInitial.toUpperCase()}</span>
-           </div>
+           </div>:<div className="gor-login-user"></div>
+       }
+            
        </div>
      
     );
