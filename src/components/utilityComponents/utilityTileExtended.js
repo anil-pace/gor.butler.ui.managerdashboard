639f1574809da04e2e5a866ae7524d6b59f255a5
arshdeep <arshdeep.m@greyorange.sg>
2018-02-27 19:50:30 +0530

    fixed the CSS for the overflow and width of the extended tile

--- src/components/utilityComponents/utilityTileExtended.js
+++ src/components/utilityComponents/utilityTileExtended.js
@@ -1,35 +1,33 @@
-import React  from 'react';
-import ReactDOM  from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { FormattedMessage } from 'react-intl';
 
-class UtilityTileExtended extends React.Component{
-	
-	render(){
+class UtilityTileExtended extends React.Component {
+
+	render() {
 		return (
-		<div className="gor-utility-tile-extended">
-			<div className="gor-utility-tile-h1">
-				<div className="gor-utility-tile-heading">
-					{this.props.tileHead}
+			<div className="gor-utility-tile-extended">
+				<div className="gor-utility-tile-h1">
+					<div className="gor-utility-tile-heading">
+						{this.props.tileHead}
+					</div>
+					{this.props.showHeaderIcon ? <div className="gor-utility-tile-icon">
+						<div className="gor-refresh-icon" onClick={this.props.onRefresh} />
+					</div> : ""}
 				</div>
-				{this.props.showHeaderIcon?<div className="gor-utility-tile-icon">
-					<div className="gor-refresh-icon" onClick={this.props.onRefresh}/>
-				</div>:""}
-			</div>
-			<div className="gor-utility-tile-body">
 				<div className="gor-utility-tile-body">
-				{this.props.children}
+					{this.props.children}
 				</div>
+				{this.props.showFooter ? <div className="gor-utility-tile-footer">
+					<div className="got-utility-btn-wrap">
+						<button className={this.props.enableButton ? "gor-download-button" : "gor-download-button gor-disable-content"} onClick={this.props.footerAction}>
+							{this.props.loading ? <div className='spinnerImage'></div> : <FormattedMessage id="utility.downDlabel" description="label for download" defaultMessage="DOWNLOAD" />}
+						</button>
+					</div>
+				</div> : ""}
 			</div>
-			{this.props.showFooter?<div className="gor-utility-tile-footer">
-				<div className="got-utility-btn-wrap">
-					<button className={this.props.enableButton?"gor-download-button":"gor-download-button gor-disable-content"} onClick={this.props.footerAction}>
-						{this.props.loading?<div className='spinnerImage'></div>:<FormattedMessage id="utility.downDlabel" description="label for download" defaultMessage="DOWNLOAD"/>}
-         			</button>
-         		</div>
-			</div>:""}
-		</div> 
 		);
 	}
 };
 
-export default UtilityTileExtended ;
+export default UtilityTileExtended;
