f22800b8efa4580925ff777798c8694e75753c4b
himanshu.s <himanshu.s@greyorange.sg>
2018-03-23 11:57:45 +0530

    grafana poc

--- src/containers/analyticsTab.js
+++ src/containers/analyticsTab.js
@@ -5,10 +5,18 @@ class AnalyticsTab extends React.Component{
 	shouldComponentUpdate(){
       return false
     }
+    componentDidMount(){
+    	let _this = this;
+    	this.ifrm.onload= function(){
+    		console.log('called');
+    		console.log(_this.ifrm.contentWindow.getElementsByClassName('navbar'));
+    	}
+    	
+    }
   render(){
 		return (
 			<div>
-				<iframe width="100%" height="500px" src="http://localhost:3000/d/NQm9Ozkiz/demo-dashboard?refresh=5s&orgId=1&from=1505980105945&to=1521618505946&theme=light"></iframe>
+				<iframe ref={(ifrm) => { this.ifrm = ifrm; }} width="100%" height="500px" src="http://localhost:3000/d/NQm9Ozkiz/demo-dashboard?refresh=5s&orgId=1&from=1505980105945&to=1521618505946&theme=light"></iframe>
 			</div>
 		);
 	}
