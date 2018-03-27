6709b9ce8938e00920167997b0ea0832ddafeba4
Raja Dey <raja.d@greyorange.sg>
2018-03-27 13:15:55 +0530

    added borrder

--- src/containers/ButlerMap.js
+++ src/containers/ButlerMap.js
@@ -16,14 +16,18 @@ class ButlerMap extends React.Component {
 
     componentDidMount() {
         const canvas = new fabric.Canvas('c', {
-            width: 500,
-            height: 500
+            width: 1000,
+            height: 500,
+            backgroundColor:'white'
           });
           var rect = new fabric.Rect({
             left: 100,
             top: 100,
             fill: 'white',
+            stroke: 'rgba(34,177,76,1)',
+            strokeWidth: 1,
             width: 80,
+           
             height: 80,
             
           });
