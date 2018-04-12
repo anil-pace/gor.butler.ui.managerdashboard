a456a947cc4fc9b649109586935316e4ace62133
Raja Dey <raja.d@greyorange.sg>
2018-04-12 12:37:39 +0000

    BSS-7926 - [MD]-Task name is not coming in title while duplicating audit.

--- src/containers/auditTab/editAudit.js
+++ src/containers/auditTab/editAudit.js
@@ -1030,7 +1030,7 @@ _onAttributeSelectionFirstTime(){
        button= <button onClick={()=>{this._createAudit("create")}} className={enableCreateAudit ? "gor-create-audit-btn" : "gor-create-audit-btn disabled"}><FormattedMessage id="audits.edit.password.button" description='Text for add audit button' 
        defaultMessage='UPDATE'/></button>
 }else if(this.props.param=='duplicate'){
-   header=<div className='gor-usr-add'><FormattedMessage id='audit.duplicate.heading' description='Heading for duplicate audit' 
+   header=<div className='gor-usr-add'><FormattedMessage id='audit.createduplicate.heading' description='Heading for duplicate audit' 
        defaultMessage='Duplicate audit'/></div>
          button=  <button onClick={()=>{this._createAudit("duplicate")}} className={enableCreateAudit ? "gor-create-audit-btn" : "gor-create-audit-btn disabled"}><FormattedMessage id="audits.add.password.button" description='Text for add audit button' 
          defaultMessage='Create audit'/></button>
