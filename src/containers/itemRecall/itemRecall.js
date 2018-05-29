214d9bbb6f4261f7123d2fe41b91564ada492a4a
Himanshu Saraswat <himanshu.s@greyorange.sg>
2018-05-29 10:26:31 +0000

    BSS-8796 [MD]-Sku details  not displayed for item recall

--- src/containers/itemRecall/itemRecall.js
+++ src/containers/itemRecall/itemRecall.js
@@ -116,20 +116,28 @@ class ItemRecall extends React.Component{
     this.props.makeAjaxCall(urlData);
  }
  _processSkuAttributes(data) {
-    var processedData=[]
+    var processedData=[],skuDetails={};
     for(let i=0,len=data.length; i<len ;i++){
-      let tuple={};
+      let tuple={},skuDetail={};
       let error_code = data[i].status===true ? "" :data[i].status.error_code;
       tuple.checked=false;
       tuple.index=i;
       tuple.visible=true;
       tuple.value=data[i].skuName;
       tuple.errorMessage = data[i].status===true ? data[i].status : this.props.intl.formatMessage(messages[error_code]);
+      if(error_code === ""){
+        skuDetail["productSku"] = data[i].skuName;
+        skuDetail["productAttributes"] =[]
+        skuDetails[data[i].skuName] = skuDetail;
+      }
       processedData.push(tuple);
     }
-    return processedData
+    return {
+      processedData:processedData,
+      skuDetails:skuDetails
+    }
   }
-  _onAttributeSelection(selectedAttributes,sku,copyPasteData){
+_onAttributeSelection(selectedAttributes,sku,copyPasteData){
     
   var skuDetails = JSON.parse(JSON.stringify(this.state.skuDetails))
   
@@ -148,9 +156,12 @@ class ItemRecall extends React.Component{
     productAttributes.push(set);
     
   }
+  tuple.productAttributes = productAttributes;
 }
-
-    tuple.productAttributes = productAttributes;
+else{
+  tuple.productAttributes = (skuDetails[skuValue]["productAttributes"]).constructor === Array ? (skuDetails[skuValue]["productAttributes"]).slice(0) : []
+}
+    
     skuDetails[skuValue] = tuple;
   }
   this.setState({
@@ -194,12 +205,13 @@ class ItemRecall extends React.Component{
       let allTuplesValid = skuAttributes.totalInvalid === 0 ? true : false;
       this.setState({
          copyPasteData:{
-          data:validatedSKUs,
+          data:validatedSKUs.processedData,
           focusedEl:"0",
           selectionStart:0
         },
         validationDoneSKU,
-        allTuplesValid
+        allTuplesValid,
+        skuDetails:validatedSKUs.skuDetails
       })
     }
   }
