0a6bc07387744a466372e355119854c18e1733ee
arshdeep <arshdeep.m@greyorange.sg>
2018-07-11 05:43:41 +0530

    Initial Commit for piwik POC

--- src/App.js
+++ src/App.js
@@ -27,6 +27,22 @@ import { FormattedMessage,FormattedNumber } from 'react-intl';
 
    constructor(props) 
    {  
+    // <!-- Matomo -->
+
+      var _paq = _paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//localhost:32802/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', '2']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+      })();
+
+    // <!-- End Matomo Code -->
+    
     super(props);
     this.state={fetchedConfig:false}
   }
