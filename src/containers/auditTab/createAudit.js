9c6e38241b69cb741279aac410997b42379ab1bd
Himanshu Saraswat <himanshu.s@greyorange.sg>
2018-08-24 12:24:29 +0000

    BSS-10032 [MD]-Issue while passing invalid location in audit by location.

--- src/containers/auditTab/createAudit.js
+++ src/containers/auditTab/createAudit.js
@@ -264,7 +264,7 @@ class CreateAudit extends React.Component{
       tuple.index=i;
       tuple.visible=true;
       tuple.value=data[i].skuName;
-      tuple.errorMessage = data[i].status===true ? data[i].status : this.props.intl.formatMessage(messages[error_code]);
+      tuple.errorMessage = data[i].status===true ? "" : this.props.intl.formatMessage(messages[error_code]);
       processedData.push(tuple);
     }
     return processedData
@@ -279,7 +279,7 @@ class CreateAudit extends React.Component{
       tuple.visible=true;
       tuple.value=data[i].name;
       let error_code = data[i].status===true ? "" :data[i].status.error_code;
-      tuple.errorMessage = data[i].status===true ? data[i].status : this.props.intl.formatMessage(messages[error_code]);
+      tuple.errorMessage = data[i].status===true ? "" : this.props.intl.formatMessage(messages[error_code]);
       processedData.push(tuple);
       if(children){
       for(let j=0;j<children.length ;j++){
@@ -288,7 +288,7 @@ class CreateAudit extends React.Component{
         child.index=j;
         child.value=children[j].name;
         let error_code = children[j].status===true ? "" : children[j].status.error_code;
-        child.errorMessage = children[j].status===true ? children[j].status : this.props.intl.formatMessage(messages[error_code]);
+        child.errorMessage = children[j].status===true ? "" : this.props.intl.formatMessage(messages[error_code]);
         processedData.push(child);
       }
     }
@@ -529,7 +529,7 @@ this.props.removeModal();
       index:stateInputList.length,
       value:"",
       visible:true,
-      errorMessage:true
+      manualEntry: true
     }
     stateInputList.push(tuple);
     if(type === "location"){
@@ -672,7 +672,7 @@ this.props.removeModal();
           processedData.push(tuple);
         }
         else if(selection === "select_all_valid"){
-          if(tuple.errorMessage.constructor === Boolean){
+          if(tuple.errorMessage ===""){
             tuple.checked = true;
           }
           else{
@@ -681,7 +681,7 @@ this.props.removeModal();
           processedData.push(tuple);
         }
         else if(selection === "select_all_invalid"){
-          if(tuple.errorMessage.constructor !== Boolean){
+          if(tuple.errorMessage !== ""){
             tuple.checked = true;
             
           }
@@ -766,7 +766,7 @@ this.props.removeModal();
             tuple.index=i;
             tuple.value=data[i];
             tuple.visible=true;
-            tuple.errorMessage = true;
+            tuple.errorMessage = "";
             processedList.push(tuple);
 
            }
@@ -1131,11 +1131,13 @@ this.props.removeModal();
                         autoFocus = {focus} 
                         updateInput={self._updateInput} 
                         index={i}
+                        key={tuple.value}
+                        manualEntry={tuple.manualEntry}
                         selectionStart = {self.state.copyPasteSKU.selectionStart}
                         allRowValid={allSKUsValid}
                         onAttributeCheck={self._onAttributeCheck}
                         checked={tuple.checked}
-                        errorMessage={!allSKUsValid ? tuple.errorMessage : true}  
+                        errorMessage={!allSKUsValid ? tuple.errorMessage : ""}  
                         value={tuple.value} placeholder={self.props.intl.formatMessage(messages.auditskuinputplaceholder)}/>
                         {allSKUsValid && attributeList.length > 0 && <SelectAttributes 
                           messages={attributeComponentMessages}
@@ -1296,11 +1298,13 @@ this.props.removeModal();
                         autoFocus = {focus} 
                         updateInput={self._updateInput} 
                         index={i}
+                        key={tuple.value}
+                        manualEntry={tuple.manualEntry}
                         selectionStart = {self.state.copyPasteLocation.selectionStart}
                         allRowValid={allLocationsValid}
                         onAttributeCheck={self._onAttributeCheck}
                         checked={tuple.checked}
-                        errorMessage={!allLocationsValid ? tuple.errorMessage : true}  
+                        errorMessage={!allLocationsValid ? tuple.errorMessage : ""}  
                         value={tuple.value} placeholder={self.props.intl.formatMessage(messages.auditlocationinputplaceholder)}/>
                       </div>)
                   }
