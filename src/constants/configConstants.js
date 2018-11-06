e72ff24811583ef903331fcf5c2b07f7391d2fa4
Raja Dey <raja.d@greyorange.sg>
2018-11-06 17:04:01 +0530

    Changes in config

--- src/constants/configConstants.js
+++ src/constants/configConstants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = '192.168.9.215'; 
+export const BASE_URL = 'localhost';
 export const PROTOCOL = 'https://';
 export const WS_PROTOCOL ="wss://";
 export const WS_URL = WS_PROTOCOL+BASE_URL+"/manager_api/wss";
