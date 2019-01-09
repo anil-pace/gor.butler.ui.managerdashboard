0ed4e08f7370c08503d15aaac4e8e820c6993f8a
himanshu.s <himanshu.s@greyorange.sg>
2019-01-09 15:39:58 +0530

    local commit

--- src/containers/auditTab/itemSearch.js
+++ src/containers/auditTab/itemSearch.js
@@ -74,17 +74,14 @@ multiPPS:{
 
  class ItemSearch extends React.Component{
 
-   constructor(props) {
-    super(props);
-     
-  } 
+   
 
 render(){
   
 
   let me=this;
   var itemsData=me.props.items;
-  var tablerowdata=[]
+  var tablerowdata=[{"initialName":{"name":"admin admin","flag":"admin admin"},"auditDetails":{"header":[38,""],"subHeader":[["PPS 1"],"Single SKU","yesterday, 18:29"],"audit_id":"UmvjVRzWiP","display_id":38},"auditProgress":{"percentage":0,"flag":false,"status":"Waiting for the operator to login"},"Status":{"resolveStatus":"","reAuditStatus":"","approvedState":""},"button":{"startButton":false,"resolveButton":false,"reAudit":false},"butoonToSHow":[{"name":"View Details","value":"viewdetails"},{"name":"Cancel","value":"cancel"},{"name":"Duplicate","value":"duplicate"},{"name":"Pause","value":"pause"}]},{"initialName":{"name":"admin admin","flag":"admin admin"},"auditDetails":{"header":[37,""],"subHeader":[["PPS 5"],"Multi location","Dec 27, 15:05"],"audit_id":"SDod3kgYuY","display_id":37},"auditProgress":{"percentage":0,"flag":false,"status":"Processing audit task"},"Status":{"resolveStatus":"","reAuditStatus":"","approvedState":""},"button":{"startButton":false,"resolveButton":false,"reAudit":false},"butoonToSHow":[{"name":"View Details","value":"viewdetails"},{"name":"Cancel","value":"cancel"},{"name":"Duplicate","value":"duplicate"},{"name":"Pause","value":"pause"}]},{"initialName":{"name":"admin admin","flag":"admin admin"},"auditDetails":{"header":[36,""],"subHeader":["Multi PPS","Single SKU","Dec 27, 11:54"],"audit_id":"iGpKxPTKRw","display_id":36},"auditProgress":{"percentage":33.333333333333336,"flag":true,"status":"1 completed out of 3"},"Status":{"resolveStatus":"","reAuditStatus":"","approvedState":""},"button":{"startButton":false,"resolveButton":false,"reAudit":false},"butoonToSHow":[{"name":"View Details","value":"viewdetails"},{"name":"Cancel","value":"cancel"},{"name":"Duplicate","value":"duplicate"},{"name":"Pause","value":"pause"}]},{"initialName":{"name":"admin admin","flag":"admin admin"},"auditDetails":{"header":[35,""],"subHeader":[["PPS 4"],"Single SKU","Dec 26, 18:59 - 19:03"],"audit_id":"wzAxam7mVj","display_id":35},"auditProgress":{"percentage":100,"flag":false,"status":"Completed"},"Status":{"resolveStatus":"","reAuditStatus":"","approvedState":""},"button":{"startButton":false,"resolveButton":false,"reAudit":false},"butoonToSHow":[{"name":"View Details","value":"viewdetails"},{"name":"Duplicate","value":"duplicate"}]},{"initialName":{"name":"admin admin","flag":"admin admin"},"auditDetails":{"header":[33,""],"subHeader":[["PPS 4"],"Single location","Dec 20, 16:14"],"audit_id":"KscU3jyYFF","display_id":33},"auditProgress":{"percentage":0,"flag":false,"status":"Paused"},"Status":{"resolveStatus":"","reAuditStatus":"","approvedState":""},"button":{"startButton":true,"resolveButton":false,"reAudit":false},"butoonToSHow":[{"name":"View Details","value":"viewdetails"},{"name":"Duplicate","value":"duplicate"}]},{"initialName":{"name":"admin admin","flag":"admin admin"},"auditDetails":{"header":[32,""],"subHeader":[["PPS 4"],"Single location","Dec 20, 15:56 - 16:11"],"audit_id":"qxtjAPF4Fh","display_id":32},"auditProgress":{"percentage":100,"flag":false,"status":"Completed"},"Status":{"resolveStatus":"","reAuditStatus":"","approvedState":""},"button":{"startButton":false,"resolveButton":false,"reAudit":false},"butoonToSHow":[{"name":"View Details","value":"viewdetails"},{"name":"Duplicate","value":"duplicate"}]},{"initialName":{"name":"admin admin","flag":"admin admin"},"auditDetails":{"header":[31,""],"subHeader":[["PPS 4"],"Single location","Dec 20, 12:23 - 12:25"],"audit_id":"yG5ELy7MV3","display_id":31},"auditProgress":{"percentage":100,"flag":false,"status":"Completed"},"Status":{"resolveStatus":"","reAuditStatus":"","approvedState":""},"button":{"startButton":false,"resolveButton":false,"reAudit":false},"butoonToSHow":[{"name":"View Details","value":"viewdetails"},{"name":"Duplicate","value":"duplicate"}]},{"initialName":{"name":"admin admin","flag":"admin admin"},"auditDetails":{"header":[30,""],"subHeader":[["PPS 5"],"Single location","Dec 19, 18:12"],"audit_id":"KmC4LdEi67","display_id":30},"auditProgress":{"percentage":0,"flag":true,"status":"0 completed out of 1"},"Status":{"resolveStatus":"","reAuditStatus":"","approvedState":""},"button":{"startButton":false,"resolveButton":false,"reAudit":false},"butoonToSHow":[{"name":"View Details","value":"viewdetails"},{"name":"Cancel","value":"cancel"},{"name":"Duplicate","value":"duplicate"},{"name":"Pause","value":"pause"}]},{"initialName":{"name":"admin admin","flag":"admin admin"},"auditDetails":{"header":[29,""],"subHeader":[["PPS 4"],"Single location","Dec 19, 18:09 - 18:11"],"audit_id":"JWZoH2NXQt","display_id":29},"auditProgress":{"percentage":100,"flag":false,"status":"Completed"},"Status":{"resolveStatus":"","reAuditStatus":"","approvedState":""},"button":{"startButton":false,"resolveButton":false,"reAudit":false},"butoonToSHow":[{"name":"View Details","value":"viewdetails"},{"name":"Duplicate","value":"duplicate"}]},{"initialName":{"name":"admin admin","flag":"admin admin"},"auditDetails":{"header":[28,""],"subHeader":[["PPS 4"],"Single location","Dec 19, 18:05 - 18:08"],"audit_id":"dUxFvKiV6i","display_id":28},"auditProgress":{"percentage":100,"flag":false,"status":"Completed"},"Status":{"resolveStatus":"","reAuditStatus":"","approvedState":""},"button":{"startButton":false,"resolveButton":false,"reAudit":false},"butoonToSHow":[{"name":"View Details","value":"viewdetails"},{"name":"Duplicate","value":"duplicate"}]}];
   var tableData=[
   {class:"auditListColumn1Style"}, 
   {class:"auditListColumn2Style"}, 
@@ -96,9 +93,22 @@ render(){
   return(
 
    <div>
+               <div className="gorToolBar auditListingToolbar">
+                <div className="gorToolBarWrap auditListingToolbarWrap">
+                    <div className="auditHeaderContainer">
+                        <label className="container">
+                            <input type="checkbox" checked={true} onChange={null} />
+                            <span className={"checkmark"}></span>
+                        </label>
+                        <span className='auditHeader'><FormattedMessage id="itemSearch.header.label"
+                                description="header label for Item Search"
+                                defaultMessage="Item Search" /></span>
+                    </div>
+                </div>
 
+            </div>
    <div className="waveListWrapper">
-   <GTable options={['table-bordered','table-itemsearch']}>
+   <GTable options={['table-bordered','table-itemsearch','table-auditListing']}>
 
 {tablerowdata && tablerowdata.length>=1?
   <GTableBody data={tablerowdata}   onScrollHandler={null}>
@@ -108,7 +118,29 @@ render(){
     <GTableRow key={idx} index={idx} data={tablerowdata} >
 
     {Object.keys(row).map(function (text, index) {
-      let visibilityStatus=tablerowdata[idx]['button'].startButton && tablerowdata[idx]['auditDetails']['subHeader'][1]!=="Wall-to-Wall"? 'visible':'hidden';
+            //let visibilityStatus=tablerowdata[idx]['button'].startButton && tablerowdata[idx]['auditDetails']['subHeader'][1]!=="Wall-to-Wall"? 'visible':'hidden';
+      return <div key={index} style={null} className={"auditListColumn1Style cell"}>
+      <label className="container checkBoxalign" style={{'visibility':"visible"}}> <input type="checkbox" id={"dummy"} checked={false}  onChange={null}/><span className="checkmark"></span></label> 
+      <NameInitial name={"dummy"} shape='round'/>:<div title="System Generated" className='systemGenerated'></div>
+      <DotSeparatorContent header={["himanshu"]} subHeader={["himanshu"]} separator={<div className="dotImage"></div>} /> 
+      <div style={{'text-align':'left'}} className="fontstyleColumn"><ProgressBar progressBarWrapperWidth="150px" progressWidth={"10"}/><div style={{'padding-top':'10px'}}>{100}</div></div>:<div style={{'text-align':'left'}}>{"Dummy"}</div>
+      <div className="column4Style"><div>{"Dummy"}</div> <div>{"Dummy"}</div><div>{"Dummy"}</div></div>
+      <div style={{'position':'relative'}}><ActionDropDown id={"dummy"} style={{float:'right'}} clickOptionBack={null} data={[{name:"Himanshu",value:'mannualassignpps'}]}>      <button className="gor-add-btn gor-listing-button">
+
+     
+       <div className="got-add-notch"></div>
+      </button>      
+      </ActionDropDown></div>
+
+      
+      <button className="gor-add-btn gor-listing-button" id={"dummy"} style={{float:'right'}}   onClick={null}>
+      
+      </button>
+       <ActionDropDown style={{right:0}} displayId = {"dummy"} id={"1"} clickOptionBack={null} data={[{name:"Himanshu",value:'mannualassignpps'}]}>
+      <div className='embeddedImage'></div>    
+      </ActionDropDown>
+
+      </div>
   
     })}
 
