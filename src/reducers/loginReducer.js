3119a1a4515002ab1e4117550c43e2bff0f37505
arshdeep <arshdeep.m@greyorange.sg>
2019-03-27 16:40:40 +0530

    BSS-13190 AD User management BOI/MD:Not able to login to butler UI with the valid ldap credentials

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -1,62 +1,80 @@
-import {LOGIN_REQUEST,SET_USERNAME,SET_TIME_OFFSET,LOGIN_REDIRECT,LOGIN_SUCCESS,LOGIN_FAILURE,LOGOUT,CONNECTION_FAILURE} from '../constants/frontEndConstants';
+import {
+  LOGIN_REQUEST,
+  SET_USERNAME,
+  SET_TIME_OFFSET,
+  LOGIN_REDIRECT,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  LOGOUT,
+  CONNECTION_FAILURE
+} from '../constants/frontEndConstants'
 /**
  * @param  {State Object}
  * @param  {Action object}
  * @return {[Object] updated state}
  */
 
-export  function authLogin(state={},action){
-	switch (action.type) {
-	
+export function authLogin(state = {}, action) {
+  switch (action.type) {
     case LOGIN_REQUEST:
       return Object.assign({}, state, {
-          "loginAuthorized":true,
-          "connectionActive":true,
-          "auth_token": sessionStorage.getItem('auth_token'),
-          "username":sessionStorage.getItem('username')
-      })    
+        loginAuthorized: true,
+        connectionActive: true,
+        auth_token: sessionStorage.getItem('auth_token'),
+        username: sessionStorage.getItem('username'),
+        grant_type: 'password',
+        action: 'LOGIN',
+        role: ['butler_supervisor', 'admin', 'manager'],
+        context: {
+          entity_id: 1,
+          app_name: 'managerdashboard_ui'
+        }
+      })
 
-	  case LOGIN_REDIRECT:
+    case LOGIN_REDIRECT:
     case LOGIN_SUCCESS:
-	   sessionStorage.setItem('auth_token', action.data.auth_token);
+      sessionStorage.setItem('auth_token', action.data.auth_token)
 
       return Object.assign({}, state, {
-          "loginAuthorized":true,
-          "connectionActive":true,
-        	"auth_token": action.data.auth_token,
-          "timeOutDuration" : action.data.duration
+        loginAuthorized: true,
+        connectionActive: true,
+        auth_token: action.data.auth_token,
+        timeOutDuration: action.data.duration
       })
 
     case LOGIN_FAILURE:
       return Object.assign({}, state, {
-          "connectionActive":true,
-          "loginAuthorized":false
+        connectionActive: true,
+        loginAuthorized: false
       })
 
     case CONNECTION_FAILURE:
       return Object.assign({}, state, {
-          "connectionActive":false,
-          "loginAuthorized":null
-      })    
+        connectionActive: false,
+        loginAuthorized: null
+      })
 
     case LOGOUT:
-     //sessionStorage.removeItem('auth_token');      
+      //sessionStorage.removeItem('auth_token');
       return Object.assign({}, state, {
-          "loginAuthorized":null,
-          "connectionActive":null,
-          "auth_token":null
+        loginAuthorized: null,
+        connectionActive: null,
+        auth_token: null
       })
     case SET_USERNAME:
-      sessionStorage.setItem('username', action.data);
+      sessionStorage.setItem('username', action.data)
       return Object.assign({}, state, {
-          "username":action.data
+        username: action.data
       })
     case SET_TIME_OFFSET:
-      sessionStorage.setItem('timeOffset', action.data.warehouse_time_zone || action.data);
+      sessionStorage.setItem(
+        'timeOffset',
+        action.data.warehouse_time_zone || action.data
+      )
       return Object.assign({}, state, {
-          "timeOffset":action.data.warehouse_time_zone || action.data
+        timeOffset: action.data.warehouse_time_zone || action.data
       })
-	  default:
-	    return state
+    default:
+      return state
   }
 }
