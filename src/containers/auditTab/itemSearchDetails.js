67778343bfe297cf8b8c9c9597e2b526d098c338
Unknown user 4c379 <sudivya.t@greyorange.com>
2019-07-17 05:59:23 +0000

    BSS-15561 Sort is not working for the role, work mode, location and login time.

--- src/containers/auditTab/itemSearchDetails.js
+++ src/containers/auditTab/itemSearchDetails.js
@@ -58,9 +58,10 @@ class ItemSearchDetails extends React.Component {
     if (data && data.length) {
       processedData.tiledata = [
         {
-          'PPS ID': data[0].status !== 'CREATED' && data[0].attributes.ppsIdList
-          ? `PPS ${data[0].attributes.ppsIdList}`
-          : '--',
+          'PPS ID':
+            data[0].status !== 'CREATED' && data[0].attributes.ppsIdList
+              ? `PPS ${data[0].attributes.ppsIdList}`
+              : '--',
           'Item Search Type': data[0].attributes.slot_list
             ? data[0].attributes.slot_list.length > 1
               ? multiLocationTxt
