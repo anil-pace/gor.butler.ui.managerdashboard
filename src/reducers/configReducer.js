e0ce907145c476e34bfd20508bef1d524ffb8c4a
arshdeep <arshdeep.m@greyorange.sg>
2019-10-01 19:34:24 +0530

    BSS-16997 HUL RC81 : Recall Items on MD is always visible although configuration is false

--- src/reducers/configReducer.js
+++ src/reducers/configReducer.js
@@ -1,16 +1,15 @@
 /**
  * Created by gaurav.m on 5/17/17.
  */
-import { RECEIVE_CONFIGS } from '../constants/frontEndConstants.js';
+import { RECEIVE_CONFIGS } from '../constants/frontEndConstants.js'
 
 export function configReducer(state = {}, action) {
-    switch (action.type) {
-        case RECEIVE_CONFIGS:
-            let config = action.data;
-            config.utility_tab.widgets.scripts.item_recall = true;
+  switch (action.type) {
+    case RECEIVE_CONFIGS:
+      let config = action.data
 
-            return Object.assign({}, state, config)
-        default:
-            return state
-    }
+      return Object.assign({}, state, config)
+    default:
+      return state
+  }
 }
