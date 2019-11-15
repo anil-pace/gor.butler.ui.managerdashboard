08cc25ca98828d091a39cc0f73eeef088c1350f3
Anil Kumar (Software Development) <anil.k@greyorange.com>
2019-11-15 08:59:17 +0000

    BSS-18278: HUL | PUT away in progress on MD with error indicator

--- src/components/utilityComponents/downloadGRTile.js
+++ src/components/utilityComponents/downloadGRTile.js
@@ -1,55 +1,64 @@
-import React from 'react';
-import { FormattedMessage, FormattedDate, defineMessages } from 'react-intl';
-import UtilityDropDown from './utilityDropdownWrap';
+import React from "react"
+import { FormattedMessage, FormattedDate, defineMessages } from "react-intl"
+import UtilityDropDown from "./utilityDropdownWrap"
 
 const messages = defineMessages({
   downloadRprtsStatusHead: {
-    id: 'utility.downloadGRN.status.heading',
-    description: 'GRN Status',
-    defaultMessage: 'GRN Status'
+    id: "utility.downloadGRN.status.heading",
+    description: "GRN Status",
+    defaultMessage: "GRN Status"
   },
   downloadGrnStnPlaceHolder: {
-    id: 'utility.downloadGRN.stnPlaceholder',
-    description: 'Enter STN Number',
-    defaultMessage: 'Enter STN Number'
+    id: "utility.downloadGRN.stnPlaceholder",
+    description: "Enter STN Number",
+    defaultMessage: "Enter STN Number"
   },
   downloadGrnSelectFormatPlaceHolder: {
-    id: 'utility.downloadGRN.formatPlaceholder',
-    description: 'Enter STN Number',
-    defaultMessage: 'Select File type'
+    id: "utility.downloadGRN.formatPlaceholder",
+    description: "Enter STN Number",
+    defaultMessage: "Select File type"
   },
   downloadGrnLink: {
-    id: 'utility.reportsHistory.clickToDownload',
-    description: 'file name',
-    defaultMessage: 'Click here to download '
+    id: "utility.reportsHistory.clickToDownload",
+    description: "file name",
+    defaultMessage: "Click here to download "
   }
-});
+})
 
 class DownloadGRNTile extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       invoiceId: null,
-      fileType: null
-    };
+      fileType: null,
+      validatedInvoice: ""
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.validatedInvoice !== this.state.validatedInvoice) {
+      this.setState({
+        validatedInvoice: nextProps.validatedInvoice
+      })
+    }
   }
 
   _changeGRNFileType(data) {
-    this.setState({ fileType: data.value });
+    this.setState({ fileType: data.value })
   }
   _getCurrentDropDownState(fileType, currentValue) {
     for (var i = fileType.length - 1; i >= 0; i--) {
       if (fileType[i].value === currentValue) {
-        return fileType[i].label;
+        return fileType[i].label
       }
     }
-    return null;
+    return null
   }
   _generateGRN() {
     if (this.props.generateReport) {
-      this.props.generateReport(this.state.fileType, this.state.invoiceId);
+      this.props.generateReport(this.state.fileType, this.state.invoiceId)
     } else {
-      throw new Error('Could not get the callback here!');
+      throw new Error("Could not get the callback here!")
     }
   }
 
@@ -58,15 +67,15 @@ class DownloadGRNTile extends React.Component {
       this.props.closeAndGenerateReport(
         this.state.fileType,
         this.state.invoiceId
-      );
+      )
     } else {
-      throw new Error('Could not get the callback here!');
+      throw new Error("Could not get the callback here!")
     }
   }
 
   _captureQuery(e) {
     if (e.target.value) {
-      this.setState({ invoiceId: e.target.value });
+      this.setState({ invoiceId: e.target.value })
     }
   }
 
@@ -75,76 +84,88 @@ class DownloadGRNTile extends React.Component {
       JSON.stringify(this.props.reportsHistory) ===
       JSON.stringify(nextProps.reportsHistory)
     ) {
-      return true;
+      return true
     } else {
-      return false;
+      return false
     }
   }
 
   render() {
-    let errorMessage = '';
-    if (this.props.validatedInvoice === true) {
+    let errorMessage = ""
+    if (
+      this.state.validatedInvoice === true ||
+      this.state.validatedInvoice === undefined
+    ) {
+      errorMessage = ""
+    } else if (this.state.validatedInvoice === false) {
       errorMessage = (
         <FormattedMessage
-          id='utility.downloadGRN.noError'
-          description=''
-          defaultMessage=''
+          id="utility.downloadGRN.putAwayInProgress"
+          description="Put away in Progress"
+          defaultMessage="Put away in Progress"
         />
-      );
-    } else if (this.props.validatedInvoice === false) {
-      errorMessage = (
-        <FormattedMessage
-          id='utility.downloadGRN.putAwayInProgress'
-          description='Put away in Progress'
-          defaultMessage='Put away in Progress'
-        />
-      );
+      )
     } else {
-      errorMessage = this.props.validatedInvoice.reason;
+      errorMessage = this.state.validatedInvoice.reason
     }
-    const fileType = [{ value: 'csv', label: 'Comma separated values (csv)' }];
+    const fileType = [{ value: "csv", label: "Comma separated values (csv)" }]
 
     let currenFileType = this.state.fileType
       ? this._getCurrentDropDownState(fileType, this.state.fileType)
-      : null;
+      : null
     return (
       <div>
-        <div className='gor-utility-invoice-h1'>
+        <div className="gor-utility-invoice-h1">
           <FormattedMessage
-            id='utility.downloadGRN.label'
-            description='STN number:'
-            defaultMessage='STN number:'
+            id="utility.downloadGRN.label"
+            description="STN number:"
+            defaultMessage="STN number:"
           />
         </div>
-        <div className='gor-audit-input-wrap gor-utility-invoice-wrap'>
+        <div className="gor-audit-input-wrap gor-utility-invoice-wrap">
           <input
-            className='gor-audit-input gor-input-ok'
+            className="gor-audit-input gor-input-ok"
             placeholder={this.context.intl.formatMessage(
               messages.downloadGrnStnPlaceHolder
             )}
             ref={node => {
-              this.invoiceId = node;
+              this.invoiceId = node
             }}
             onChange={this._captureQuery.bind(this)}
           />
-          {this.props.validatedInvoice.code === 'ERR002' ||
-          this.props.validatedInvoice === false ? (
-            <div className='gor-login-error' />
+          {this.state.validatedInvoice ? (
+            this.state.validatedInvoice.code === "ERR002" ||
+            this.state.validatedInvoice === false ? (
+              <div className="gor-login-error" />
+            ) : (
+              ""
+            )
           ) : (
-            ''
+            ""
+          )}
+          {this.state.validatedInvoice === false ? (
+            <div className="gor-login-error" />
+          ) : (
+            ""
           )}
         </div>
-        {this.props.validatedInvoice.code === 'ERR002' ||
-        this.props.validatedInvoice === false ? (
-          <div className='gor-sku-error gor-utility-error-invoice'>
+        {this.state.validatedInvoice ? (
+          this.state.validatedInvoice.code === "ERR002" ||
+          this.state.validatedInvoice === false ? (
+            <div className="gor-sku-error gor-utility-error-invoice">
+              {errorMessage}
+            </div>
+          ) : (
+            ""
+          )
+        ) : (
+          <div className="gor-sku-error gor-utility-error-invoice">
             {errorMessage}
           </div>
-        ) : (
-          ''
         )}
         <UtilityDropDown
           items={fileType}
-          dropdownLabel='File format'
+          dropdownLabel="File format"
           placeHolderText={this.context.intl.formatMessage(
             messages.downloadGrnSelectFormatPlaceHolder
           )}
@@ -152,32 +173,32 @@ class DownloadGRNTile extends React.Component {
           currentState={currenFileType}
         />
 
-        <div className='gor-utility-btn-wrap'>
+        <div className="gor-utility-btn-wrap">
           <button
             onClick={this._closeAndGenerateGRN.bind(this)}
             className={
               this.state.invoiceId && this.state.fileType
-                ? 'gor-download-button'
-                : 'gor-download-button gor-disable-content'
+                ? "gor-download-button"
+                : "gor-download-button gor-disable-content"
             }
           >
             <label>
               <FormattedMessage
-                id='utility.cloaseAndDownloadGRN.head'
-                description='Close and Generate Report'
-                defaultMessage='Close and Generate Report'
+                id="utility.cloaseAndDownloadGRN.head"
+                description="Close and Generate Report"
+                defaultMessage="Close and Generate Report"
               />
             </label>
           </button>
-          <div style={{ height: '5px' }}> </div>
+          <div style={{ height: "5px" }}> </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
 DownloadGRNTile.contextTypes = {
   intl: React.PropTypes.object.isRequired
-};
+}
 
-export default DownloadGRNTile;
+export default DownloadGRNTile
--- src/containers/reportsTab/misc.js
+++ src/containers/reportsTab/misc.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import UtilityTile from '../../components/utilityComponents/utilityTile';
-import DownloadReportTile from '../../components/utilityComponents/downloadReportsTile';
-import DownloadGRNTile from '../../components/utilityComponents/downloadGRTile';
+import React from "react"
+import UtilityTile from "../../components/utilityComponents/utilityTile"
+import DownloadReportTile from "../../components/utilityComponents/downloadReportsTile"
+import DownloadGRNTile from "../../components/utilityComponents/downloadGRTile"
 import {
   INVENTORY_REPORT_URL,
   GR_REPORT_URL,
   CLOSE_GR_REPORT_URL
-} from "../../constants/configConstants";
-import { connect } from "react-redux";
-import { getGRdata, validateInvoiceID } from "../../actions/utilityActions";
-import { setInventoryReportSpinner } from "../../actions/spinnerAction";
+} from "../../constants/configConstants"
+import { connect } from "react-redux"
+import { getGRdata, validateInvoiceID } from "../../actions/utilityActions"
+import { setInventoryReportSpinner } from "../../actions/spinnerAction"
 import {
   GET,
   POST,
@@ -17,49 +17,49 @@ import {
   GR_REPORT_RESPONSE,
   INVENTORY_REPORT_RESPONSE,
   WS_ONSEND
-} from '../../constants/frontEndConstants';
+} from "../../constants/frontEndConstants"
 
-import { defineMessages } from 'react-intl';
+import { defineMessages } from "react-intl"
 import {
   updateSubscriptionPacket,
   setWsAction
-} from "./../../actions/socketActions";
-import { wsOverviewData } from "./../../constants/initData.js";
-import { setLoginSpinner } from '../../actions/loginAction';
+} from "./../../actions/socketActions"
+import { wsOverviewData } from "./../../constants/initData.js"
+import { setLoginSpinner } from "../../actions/loginAction"
 
 //Mesages for internationalization
 const messages = defineMessages({
   downloadReportsHead: {
-    id: 'utility.downloadReport.head',
-    description: 'Download Reports',
-    defaultMessage: 'Download Reports'
+    id: "utility.downloadReport.head",
+    description: "Download Reports",
+    defaultMessage: "Download Reports"
   },
   goodsRcvdNotesHead: {
-    id: 'utility.goodsRcvdNotes.head',
-    description: 'Goods Received Notes',
-    defaultMessage: 'Goods Received Notes'
+    id: "utility.goodsRcvdNotes.head",
+    description: "Goods Received Notes",
+    defaultMessage: "Goods Received Notes"
   }
-});
+})
 
 class UtilityTab extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       reportState: {},
       grnState: {},
       utilityTabRefreshed: null
-    };
+    }
   }
 
   _closeAndGenerateReport(reqFileType, invoiceId) {
-    var fileType = "csv";
+    var fileType = "csv"
     if (reqFileType) {
-      fileType = reqFileType;
+      fileType = reqFileType
     }
     if (!invoiceId) {
-      throw new Error("Did not receive the Invoice id for GRN generation!");
+      throw new Error("Did not receive the Invoice id for GRN generation!")
     }
-    var url = CLOSE_GR_REPORT_URL + invoiceId;
+    var url = CLOSE_GR_REPORT_URL + invoiceId
     let data = {
       url: url,
       method: POST,
@@ -67,59 +67,58 @@ class UtilityTab extends React.Component {
       token: this.props.auth_token,
       cause: GR_REPORT_RESPONSE,
       accept: APP_JSON
-    };
+    }
     this.props.setLoginSpinner(true)
-    this.props.getGRdata(data);
+    this.props.getGRdata(data)
   }
 
   _generateReport(reqFileType) {
-    let fileType = 'csv';
+    let fileType = "csv"
     if (reqFileType) {
-      fileType = reqFileType;
+      fileType = reqFileType
     }
 
     let url =
       INVENTORY_REPORT_URL +
-      '&user=' +
+      "&user=" +
       this.props.username +
-      '&sync=false&format=' +
-      fileType;
+      "&sync=false&format=" +
+      fileType
     let data = {
       url: url,
       method: POST,
       token: this.props.auth_token,
-      responseType: 'arraybuffer',
+      responseType: "arraybuffer",
       cause: INVENTORY_REPORT_RESPONSE
-    };
-    this.props.setInventoryReportSpinner(true);
-    this.props.getGRdata(data);
+    }
+    this.props.setInventoryReportSpinner(true)
+    this.props.getGRdata(data)
   }
 
   _changeReportFileType(data) {
-    var newState = this.state.reportState;
-    newState.fileType = data.value;
-    this.setState({ reportState: newState });
+    var newState = this.state.reportState
+    newState.fileType = data.value
+    this.setState({ reportState: newState })
   }
 
   _generateGRN(reqFileType, invoiceId) {
-    var fileType = 'csv';
+    var fileType = "csv"
     if (reqFileType) {
-      fileType = reqFileType;
+      fileType = reqFileType
     }
     if (!invoiceId) {
-      throw new Error('Did not receive the Invoice id for GRN generation!');
+      throw new Error("Did not receive the Invoice id for GRN generation!")
     }
-    var url =
-      GR_REPORT_URL + '/' + invoiceId + '?sync=false&format=' + fileType;
+    var url = GR_REPORT_URL + "/" + invoiceId + "?sync=false&format=" + fileType
     let data = {
       url: url,
       method: GET,
       token: this.props.auth_token,
       cause: GR_REPORT_RESPONSE,
-      responseType: 'arraybuffer'
-    };
-    this.props.getGRdata(data);
-    this.props.validateInvoiceID(false);
+      responseType: "arraybuffer"
+    }
+    this.props.getGRdata(data)
+    this.props.validateInvoiceID(false)
   }
 
   componentWillReceiveProps(nextProps, nextState) {
@@ -129,43 +128,43 @@ class UtilityTab extends React.Component {
     ) {
       this.setState({
         utilityTabRefreshed: nextProps.utilityTabRefreshed
-      });
-      this._subscribeData();
+      })
+      this._subscribeData()
     }
   }
 
   _subscribeData() {
-    let updatedWsSubscription = this.props.wsSubscriptionData;
-    this.props.initDataSentCall(updatedWsSubscription['default']);
-    this.props.updateSubscriptionPacket(updatedWsSubscription);
+    let updatedWsSubscription = this.props.wsSubscriptionData
+    this.props.initDataSentCall(updatedWsSubscription["default"])
+    this.props.updateSubscriptionPacket(updatedWsSubscription)
   }
 
   render() {
-    let show_gr_report = false;
-    let show_inventory_report = false;
+    let show_gr_report = false
+    let show_inventory_report = false
 
     try {
-      show_gr_report = this.props.config.utility_tab.widgets.gr_report;
+      show_gr_report = this.props.config.utility_tab.widgets.gr_report
     } catch (ex) {
       //Do nothing
     }
 
     try {
       show_inventory_report = this.props.config.utility_tab.widgets.reports
-        .inventory_report;
+        .inventory_report
     } catch (ex) {
       //Do nothing
     }
 
     return (
-      <div style={{ "display": "flex", "flex-direction": "row" }}>
+      <div style={{ display: "flex", "flex-direction": "row" }}>
         {show_inventory_report ? (
-          <div style={{ "width": "25%" }}>
+          <div style={{ width: "25%" }}>
             <UtilityTile
               tileHead={this.context.intl.formatMessage(
                 messages.downloadReportsHead
               )}
-              additionalClass='width-100'
+              additionalClass="width-100"
             >
               <DownloadReportTile
                 generateReport={this._generateReport.bind(this)}
@@ -175,12 +174,12 @@ class UtilityTab extends React.Component {
           </div>
         ) : null}
         {show_gr_report ? (
-          <div style={{ "width": "25%", marginLeft: "2%" }}>
+          <div style={{ width: "25%", marginLeft: "2%" }}>
             <UtilityTile
               tileHead={this.context.intl.formatMessage(
                 messages.goodsRcvdNotesHead
               )}
-              additionalClass='width-100'
+              additionalClass="width-100"
               showFooter={false}
             >
               <DownloadGRNTile
@@ -193,7 +192,7 @@ class UtilityTab extends React.Component {
           </div>
         ) : null}
       </div>
-    );
+    )
   }
 }
 
@@ -202,43 +201,40 @@ function mapStateToProps(state, ownProps) {
     auth_token: state.authLogin.auth_token,
     timeOffset: state.authLogin.timeOffset,
     utilityTabRefreshed: state.utilityValidations.utilityTabRefreshed,
-    validatedInvoice: state.utilityValidations.invalidInvoice || false,
+    validatedInvoice: state.utilityValidations.invalidInvoice,
     newFileUploaded: state.utilityValidations.newFileUploaded,
     wsSubscriptionData:
       state.recieveSocketActions.socketDataSubscriptionPacket || wsOverviewData,
     socketAuthorized: state.recieveSocketActions.socketAuthorized,
     username: state.authLogin.username,
     config: state.config || {}
-  };
+  }
 }
 
-var mapDispatchToProps = function (dispatch) {
+var mapDispatchToProps = function(dispatch) {
   return {
-    getGRdata: function (data) {
-      dispatch(getGRdata(data));
+    getGRdata: function(data) {
+      dispatch(getGRdata(data))
     },
-    validateInvoiceID: function (data) {
-      dispatch(validateInvoiceID(data));
+    validateInvoiceID: function(data) {
+      dispatch(validateInvoiceID(data))
     },
-    setInventoryReportSpinner: function (data) {
-      dispatch(setInventoryReportSpinner(data));
+    setInventoryReportSpinner: function(data) {
+      dispatch(setInventoryReportSpinner(data))
     },
-    updateSubscriptionPacket: function (data) {
-      dispatch(updateSubscriptionPacket(data));
+    updateSubscriptionPacket: function(data) {
+      dispatch(updateSubscriptionPacket(data))
     },
-    initDataSentCall: function (data) {
-      dispatch(setWsAction({ type: WS_ONSEND, data: data }));
+    initDataSentCall: function(data) {
+      dispatch(setWsAction({ type: WS_ONSEND, data: data }))
     },
-    setLoginSpinner: function (data) {
+    setLoginSpinner: function(data) {
       dispatch(setLoginSpinner(data))
     }
-  };
-};
+  }
+}
 UtilityTab.contextTypes = {
   intl: React.PropTypes.object.isRequired
-};
+}
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(UtilityTab);
+export default connect(mapStateToProps, mapDispatchToProps)(UtilityTab)
