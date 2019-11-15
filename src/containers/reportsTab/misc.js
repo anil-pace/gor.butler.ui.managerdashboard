13a6df7b375de62f433b467a4c6aa60ce2b91c7c
Anil Kumar (Software Development) <anil.k@greyorange.com>
2019-11-15 13:54:37 +0000

    BSS-19016: HUL | "Upload master data" button not working on MD on click

--- src/containers/reportsTab/misc.js
+++ src/containers/reportsTab/misc.js
@@ -1,15 +1,15 @@
-import React from "react";
-import UtilityTile from "../../components/utilityComponents/utilityTile";
-import DownloadReportTile from "../../components/utilityComponents/downloadReportsTile";
-import DownloadGRNTile from "../../components/utilityComponents/downloadGRTile";
+import React from "react"
+import UtilityTile from "../../components/utilityComponents/utilityTile"
+import DownloadReportTile from "../../components/utilityComponents/downloadReportsTile"
+import DownloadGRNTile from "../../components/utilityComponents/downloadGRTile"
 import {
   INVENTORY_REPORT_URL,
   GR_REPORT_URL,
   CLOSE_GR_REPORT_URL
-} from "../../constants/configConstants";
-import { connect } from "react-redux";
-import { getGRdata, validateInvoiceID } from "../../actions/utilityActions";
-import { setInventoryReportSpinner } from "../../actions/spinnerAction";
+} from "../../constants/configConstants"
+import { connect } from "react-redux"
+import { getGRdata, validateInvoiceID } from "../../actions/utilityActions"
+import { setInventoryReportSpinner } from "../../actions/spinnerAction"
 import {
   GET,
   POST,
@@ -17,15 +17,15 @@ import {
   GR_REPORT_RESPONSE,
   INVENTORY_REPORT_RESPONSE,
   WS_ONSEND
-} from "../../constants/frontEndConstants";
+} from "../../constants/frontEndConstants"
 
-import { defineMessages } from "react-intl";
+import { defineMessages } from "react-intl"
 import {
   updateSubscriptionPacket,
   setWsAction
-} from "./../../actions/socketActions";
-import { wsOverviewData } from "./../../constants/initData.js";
-import { setLoginSpinner } from "../../actions/loginAction";
+} from "./../../actions/socketActions"
+import { wsOverviewData } from "./../../constants/initData.js"
+import { setLoginSpinner } from "../../actions/loginAction"
 
 //Mesages for internationalization
 const messages = defineMessages({
@@ -39,27 +39,27 @@ const messages = defineMessages({
     description: "Goods Received Notes",
     defaultMessage: "Goods Received Notes"
   }
-});
+})
 
 class UtilityTab extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       reportState: {},
       grnState: {},
       utilityTabRefreshed: null
-    };
+    }
   }
 
   _closeAndGenerateReport(reqFileType, invoiceId) {
-    var fileType = "csv";
+    var fileType = "csv"
     if (reqFileType) {
-      fileType = reqFileType;
+      fileType = reqFileType
     }
     if (!invoiceId) {
-      throw new Error("Did not receive the Invoice id for GRN generation!");
+      throw new Error("Did not receive the Invoice id for GRN generation!")
     }
-    var url = CLOSE_GR_REPORT_URL + invoiceId;
+    var url = CLOSE_GR_REPORT_URL + invoiceId
     let data = {
       url: url,
       method: POST,
@@ -67,15 +67,15 @@ class UtilityTab extends React.Component {
       token: this.props.auth_token,
       cause: GR_REPORT_RESPONSE,
       accept: APP_JSON
-    };
-    this.props.setLoginSpinner(true);
-    this.props.getGRdata(data);
+    }
+    this.props.setLoginSpinner(true)
+    this.props.getGRdata(data)
   }
 
   _generateReport(reqFileType) {
-    let fileType = "csv";
+    let fileType = "csv"
     if (reqFileType) {
-      fileType = reqFileType;
+      fileType = reqFileType
     }
 
     let url =
@@ -83,43 +83,42 @@ class UtilityTab extends React.Component {
       "&user=" +
       this.props.username +
       "&sync=false&format=" +
-      fileType;
+      fileType
     let data = {
       url: url,
       method: POST,
       token: this.props.auth_token,
       responseType: "arraybuffer",
       cause: INVENTORY_REPORT_RESPONSE
-    };
-    this.props.setInventoryReportSpinner(true);
-    this.props.getGRdata(data);
+    }
+    this.props.setInventoryReportSpinner(true)
+    this.props.getGRdata(data)
   }
 
   _changeReportFileType(data) {
-    var newState = this.state.reportState;
-    newState.fileType = data.value;
-    this.setState({ reportState: newState });
+    var newState = this.state.reportState
+    newState.fileType = data.value
+    this.setState({ reportState: newState })
   }
 
   _generateGRN(reqFileType, invoiceId) {
-    var fileType = "csv";
+    var fileType = "csv"
     if (reqFileType) {
-      fileType = reqFileType;
+      fileType = reqFileType
     }
     if (!invoiceId) {
-      throw new Error("Did not receive the Invoice id for GRN generation!");
+      throw new Error("Did not receive the Invoice id for GRN generation!")
     }
-    var url =
-      GR_REPORT_URL + "/" + invoiceId + "?sync=false&format=" + fileType;
+    var url = GR_REPORT_URL + "/" + invoiceId + "?sync=false&format=" + fileType
     let data = {
       url: url,
       method: GET,
       token: this.props.auth_token,
       cause: GR_REPORT_RESPONSE,
       responseType: "arraybuffer"
-    };
-    this.props.getGRdata(data);
-    this.props.validateInvoiceID(false);
+    }
+    this.props.getGRdata(data)
+    this.props.validateInvoiceID(false)
   }
 
   componentWillReceiveProps(nextProps, nextState) {
@@ -129,30 +128,30 @@ class UtilityTab extends React.Component {
     ) {
       this.setState({
         utilityTabRefreshed: nextProps.utilityTabRefreshed
-      });
-      this._subscribeData();
+      })
+      this._subscribeData()
     }
   }
 
   _subscribeData() {
-    let updatedWsSubscription = this.props.wsSubscriptionData;
-    this.props.initDataSentCall(updatedWsSubscription["default"]);
-    this.props.updateSubscriptionPacket(updatedWsSubscription);
+    let updatedWsSubscription = this.props.wsSubscriptionData
+    this.props.initDataSentCall(updatedWsSubscription["default"])
+    this.props.updateSubscriptionPacket(updatedWsSubscription)
   }
 
   render() {
-    let show_gr_report = false;
-    let show_inventory_report = false;
+    let show_gr_report = false
+    let show_inventory_report = false
 
     try {
-      show_gr_report = this.props.config.utility_tab.widgets.gr_report;
+      show_gr_report = this.props.config.utility_tab.widgets.gr_report
     } catch (ex) {
       //Do nothing
     }
 
     try {
       show_inventory_report = this.props.config.utility_tab.widgets.reports
-        .inventory_report;
+        .inventory_report
     } catch (ex) {
       //Do nothing
     }
@@ -193,7 +192,7 @@ class UtilityTab extends React.Component {
           </div>
         ) : null}
       </div>
-    );
+    )
   }
 }
 
@@ -209,33 +208,33 @@ function mapStateToProps(state, ownProps) {
     socketAuthorized: state.recieveSocketActions.socketAuthorized,
     username: state.authLogin.username,
     config: state.config || {}
-  };
+  }
 }
 
 var mapDispatchToProps = function(dispatch) {
   return {
     getGRdata: function(data) {
-      dispatch(getGRdata(data));
+      dispatch(getGRdata(data))
     },
     validateInvoiceID: function(data) {
-      dispatch(validateInvoiceID(data));
+      dispatch(validateInvoiceID(data))
     },
     setInventoryReportSpinner: function(data) {
-      dispatch(setInventoryReportSpinner(data));
+      dispatch(setInventoryReportSpinner(data))
     },
     updateSubscriptionPacket: function(data) {
-      dispatch(updateSubscriptionPacket(data));
+      dispatch(updateSubscriptionPacket(data))
     },
     initDataSentCall: function(data) {
-      dispatch(setWsAction({ type: WS_ONSEND, data: data }));
+      dispatch(setWsAction({ type: WS_ONSEND, data: data }))
     },
     setLoginSpinner: function(data) {
-      dispatch(setLoginSpinner(data));
+      dispatch(setLoginSpinner(data))
     }
-  };
-};
+  }
+}
 UtilityTab.contextTypes = {
   intl: React.PropTypes.object.isRequired
-};
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(UtilityTab);
+export default connect(mapStateToProps, mapDispatchToProps)(UtilityTab)
