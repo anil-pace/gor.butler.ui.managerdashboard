098e7c48bfe347307f854bba5fa81da65944e060
Shikher Singhal <shikher.s@greyorange.com>
2020-05-06 17:46:47 +0000

    Merged in BSS-23241_op_debug (pull request #1067)
    BSS-23241
    
    * BSS-23241
    
    * BSS-23241
    
    * BSS-23241
    
    Approved-by: Shikher Singhal <shikher.s@greyorange.com>

--- src/components/tableFilter/filterInputFieldWrap.js
+++ src/components/tableFilter/filterInputFieldWrap.js
@@ -1,39 +1,91 @@
-import React  from 'react';
-import FilterInputField from './filterInputField';
+import React from "react"
+import FilterInputField from "./filterInputField"
+import { ORDER_NO, MAX_SIZE_INPUT } from "../../constants/frontEndConstants"
+import { check } from "graphql-anywhere"
 
-class FilterInputFieldWrap extends React.Component{
-	
- _processInputField() {
-  var totalInput=this.props.inputText, inputFieldWrap=[],  by2value=false,type="text";
-  var textboxStatus=this.props.textboxStatus==null || typeof(this.props.textboxStatus)=="string"?this.props.textboxStatus?JSON.parse(this.props.textboxStatus):JSON.parse('{"name":["AUDIT TASK ID"]}'):this.props.textboxStatus ||{};
+class FilterInputFieldWrap extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      showError: false
+    }
+  }
+  isValidInput(key, valueobj) {
+    if (key === ORDER_NO) {
+      let ids = valueobj[key] ? valueobj[key].split(",") : ""
+      if (ids.length > MAX_SIZE_INPUT) {
+        this.props.disableApplyButton(true)
+        return true
+      } else {
+        this.props.disableApplyButton(false)
+        return false
+      }
+    }
+  }
 
-  var length=totalInput.length;
+  _processInputField() {
+    var totalInput = this.props.inputText,
+      inputFieldWrap = [],
+      by2value = false,
+      type = "text"
+    var textboxStatus =
+      this.props.textboxStatus == null ||
+      typeof this.props.textboxStatus == "string"
+        ? this.props.textboxStatus
+          ? JSON.parse(this.props.textboxStatus)
+          : JSON.parse('{"name":["AUDIT TASK ID"]}')
+        : this.props.textboxStatus || {}
 
-  for (let i=0; i < length; i++) {
-    let flag=false;
-    let type="text";
-    type=totalInput[i].type?totalInput[i].type:"text";
-    by2value=totalInput[i].by2value?true:false;
-    if(textboxStatus.name && (totalInput[i].value=="SPECIFIC_SKU_ID" || totalInput[i].value=="SPECIFIC_LOCATION_ID")){
-     flag=((textboxStatus.name).indexOf(totalInput[i].value))!==-1?false:true;
-   }
-   var inputValue=(flag?"":this.props.inputValue[totalInput[i].value]);
-   var inputField=<FilterInputField key={i} by2value={by2value} type={type} inputText={totalInput[i]} placeholder={this.props.placeholder} handleInputText={this.props.handleInputText} inputValue={inputValue} flag={flag}/>;
-   inputFieldWrap.push(inputField)
- }
- return inputFieldWrap;
+    var length = totalInput.length
+
+    for (let i = 0; i < length; i++) {
+      let flag = false
+      let type = "text"
+      type = totalInput[i].type ? totalInput[i].type : "text"
+      by2value = totalInput[i].by2value ? true : false
+      if (
+        textboxStatus.name &&
+        (totalInput[i].value == "SPECIFIC_SKU_ID" ||
+          totalInput[i].value == "SPECIFIC_LOCATION_ID")
+      ) {
+        flag =
+          textboxStatus.name.indexOf(totalInput[i].value) !== -1 ? false : true
+      }
+      var inputValue = flag ? "" : this.props.inputValue[totalInput[i].value]
+      var isNotValid = this.isValidInput(
+        totalInput[i].value,
+        this.props.inputValue
+      )
+      var inputField = (
+        <span>
+          <FilterInputField
+            key={i}
+            by2value={by2value}
+            type={type}
+            inputText={totalInput[i]}
+            placeholder={this.props.placeholder}
+            handleInputText={this.props.handleInputText}
+            inputValue={inputValue}
+            flag={flag}
+          />
+          {isNotValid ? (
+            <div div className="gor-filter-input-warning">
+              {`Max Allowed Limit is ${MAX_SIZE_INPUT} IDs`}
+            </div>
+          ) : (
+            ""
+          )}
+        </span>
+      )
+      inputFieldWrap.push(inputField)
+    }
+    return inputFieldWrap
+  }
+
+  render() {
+    var inputFields = this._processInputField()
+    return <div>{inputFields}</div>
+  }
 }
-render(){
-  var inputFields=this._processInputField()
-  return (
-          <div>
-          {inputFields}
-          </div>
-          );
-}
-}; 
 
-export default FilterInputFieldWrap;
-
-
-
+export default FilterInputFieldWrap
