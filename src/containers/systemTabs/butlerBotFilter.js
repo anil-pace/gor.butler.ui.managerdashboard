fcd4babbe29ef09ce3a0cb2add71f823dcfd6658
shikher.s@greyorange.com <GREYORANGE\shikher.s@ggn001253.greyorange.com>
2020-06-01 13:46:33 +0530

    BSS-24290

--- src/containers/systemTabs/butlerBotFilter.js
+++ src/containers/systemTabs/butlerBotFilter.js
@@ -1,32 +1,33 @@
-import React  from 'react';
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import Filter from '../../components/tableFilter/filter';
-import {showTableFilter,filterApplied,butlerfilterState,toggleBotButton} from '../../actions/filterAction';
-import {updateSubscriptionPacket} from '../../actions/socketActions';
-import { connect } from 'react-redux'; 
+import { showTableFilter, filterApplied, butlerfilterState, toggleBotButton } from '../../actions/filterAction';
+import { updateSubscriptionPacket } from '../../actions/socketActions';
+import { connect } from 'react-redux';
 import FilterInputFieldWrap from '../../components/tableFilter/filterInputFieldWrap';
 import FilterTokenWrap from '../../components/tableFilter/filterTokenContainer';
-import {handelTokenClick, handleInputQuery} from '../../components/tableFilter/tableFilterCommonFunctions';
-import {setButlerFilterSpinner}  from '../../actions/spinnerAction';
-import {hashHistory} from 'react-router';
-import {graphql, withApollo, compose} from "react-apollo";
+import { handelTokenClick, handleInputQuery } from '../../components/tableFilter/tableFilterCommonFunctions';
+import { setButlerFilterSpinner } from '../../actions/spinnerAction';
+import { hashHistory } from 'react-router';
+import { graphql, withApollo, compose } from "react-apollo";
 import gql from 'graphql-tag'
-class ButlerBotFilter extends React.Component{
-	constructor(props) 
-	{
-    	super(props);
-        this.state={tokenSelected: {"STATUS":["any"], "MODE":["any"]}, searchQuery: {},
-                      defaultToken: {"STATUS":["any"], "MODE":["any"]}}; 
+class ButlerBotFilter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tokenSelected: { "STATUS": ["any"], "MODE": ["any"] }, searchQuery: {},
+            defaultToken: { "STATUS": ["any"], "MODE": ["any"] }
+        };
         this._closeFilter = this._closeFilter.bind(this);
         this._applyFilter = this._applyFilter.bind(this);
         this._clearFilter = this._clearFilter.bind(this);
     }
 
-    componentWillReceiveProps(nextProps){
+    componentWillReceiveProps(nextProps) {
 
 
 
-        if(nextProps.filterState && JSON.stringify(this.state)!==JSON.stringify(nextProps.filterState)){
+        if (nextProps.filterState && JSON.stringify(this.state) !== JSON.stringify(nextProps.filterState)) {
             /**
              * As soon as the properties are changed from
              * the Show All list, the state will be
@@ -38,159 +39,159 @@ class ButlerBotFilter extends React.Component{
         /**
          * Hide the filter as soon as data in the list get updated.
          */
-        if(nextProps.butlerData.length>0 && JSON.stringify(nextProps.butlerData)!==JSON.stringify(this.props.butlerData)){
+        if (nextProps.butlerData.length > 0 && JSON.stringify(nextProps.butlerData) !== JSON.stringify(this.props.butlerData)) {
             this.props.showBotsFilter(false);
         }
     }
     _closeFilter() {
         this.props.showBotsFilter(false);
-    }	
-
-    _processButlerSearchField(){    
-        const temp=[{value:"BOT_ID", label:<FormattedMessage id="butletbot.inputField.id" defaultMessage="BOT ID"/>}, 
-                    {value:"SPECIFIC_LOCATION_ZONE", label:<FormattedMessage id="butletbot.inputField.sku" defaultMessage="SPECIFIC LOCATION/ZONE"/>}];
-        let inputValue=this.state.searchQuery;
-        let inputField=<FilterInputFieldWrap inputText={temp} handleInputText={this._handleInputQuery.bind(this)} inputValue={inputValue}/>
-        return inputField;           
     }
- 
+
+    _processButlerSearchField() {
+        const temp = [{ value: "BOT_ID", label: <FormattedMessage id="butletbot.inputField.id" defaultMessage="BOT ID" /> },
+        { value: "SPECIFIC_LOCATION_ZONE", label: <FormattedMessage id="butletbot.inputField.sku" defaultMessage="SPECIFIC LOCATION/ZONE" /> }];
+        let inputValue = this.state.searchQuery;
+        let inputField = <FilterInputFieldWrap inputText={temp} handleInputText={this._handleInputQuery.bind(this)} inputValue={inputValue} />
+        return inputField;
+    }
+
     _processFilterToken() {
-        let tokenFieldC1={value:"STATUS", label:<FormattedMessage id="butletbot.tokenfield.STATUS" defaultMessage="STATUS"/>};
-        let tokenFieldC2={value:"MODE", label:<FormattedMessage id="butletbot.tokenfield.MODE" defaultMessage="MODE"/>}; 
-        const labelC1=[
-                    { value: 'any', label:<FormattedMessage id="butletbot.token1.all" defaultMessage="Any"/> },
-                    { value: 'error', label:<FormattedMessage id="butletbot.token1.error" defaultMessage="Error"/> },
-                    { value: 'online', label:<FormattedMessage id="butletbot.token1.Online" defaultMessage="Online"/> },
-                    { value: 'offline', label:<FormattedMessage id="butletbot.token1.Offline" defaultMessage="Offline"/> }
-                    ];
-        const labelC2=[
-                    { value: 'any', label:<FormattedMessage id="butletbot.token2.any" defaultMessage="Any"/> },
-                    { value: 'picktask', label:<FormattedMessage id="butletbot.token2.pick" defaultMessage="Pick"/>},
-                    { value: 'puttask', label:<FormattedMessage id="butletbot.token2.put" defaultMessage="Put"/> },
-                    { value: 'audittask', label:<FormattedMessage id="butletbot.token2.audit" defaultMessage="Audit"/> },
-                    { value: 'chargetask', label:<FormattedMessage id="butletbot.token2.charging" defaultMessage="Charging"/>},
-                    { value: 'not set', label:<FormattedMessage id="butletbot.token2.notSet" defaultMessage="Not set"/> }];
-        let selectedToken= this.state.tokenSelected;
-        let column1=<FilterTokenWrap field={tokenFieldC2} tokenCallBack={this._handelTokenClick.bind(this)} label={labelC2} selectedToken={selectedToken}/>;
-        let column2=<FilterTokenWrap field={tokenFieldC1} tokenCallBack={this._handelTokenClick.bind(this)} label={labelC1} selectedToken={selectedToken}/>;
-        let columnDetail={column1token:column1, column2token:column2};
+        let tokenFieldC1 = { value: "STATUS", label: <FormattedMessage id="butletbot.tokenfield.STATUS" defaultMessage="STATUS" /> };
+        let tokenFieldC2 = { value: "MODE", label: <FormattedMessage id="butletbot.tokenfield.MODE" defaultMessage="MODE" /> };
+        const labelC1 = [
+            { value: 'any', label: <FormattedMessage id="butletbot.token1.all" defaultMessage="Any" /> },
+            { value: 'error', label: <FormattedMessage id="butletbot.token1.error" defaultMessage="Error" /> },
+            { value: 'online', label: <FormattedMessage id="butletbot.token1.Online" defaultMessage="Online" /> },
+            { value: 'offline', label: <FormattedMessage id="butletbot.token1.Offline" defaultMessage="Offline" /> }
+        ];
+        const labelC2 = [
+            { value: 'any', label: <FormattedMessage id="butletbot.token2.any" defaultMessage="Any" /> },
+            { value: 'picktask', label: <FormattedMessage id="butletbot.token2.pick" defaultMessage="Pick" /> },
+            { value: 'puttask', label: <FormattedMessage id="butletbot.token2.put" defaultMessage="Put" /> },
+            { value: 'audittask', label: <FormattedMessage id="butletbot.token2.audit" defaultMessage="Audit" /> },
+            { value: 'chargetask', label: <FormattedMessage id="butletbot.token2.charging" defaultMessage="Charging" /> },
+            { value: 'not set', label: <FormattedMessage id="butletbot.token2.notSet" defaultMessage="Not set" /> }];
+        let selectedToken = this.state.tokenSelected;
+        let column1 = <FilterTokenWrap field={tokenFieldC2} tokenCallBack={this._handelTokenClick.bind(this)} label={labelC2} selectedToken={selectedToken} />;
+        let column2 = <FilterTokenWrap field={tokenFieldC1} tokenCallBack={this._handelTokenClick.bind(this)} label={labelC1} selectedToken={selectedToken} />;
+        let columnDetail = { column1token: column1, column2token: column2 };
         return columnDetail;
     }
 
-    _handelTokenClick(field,value,state) {
-        this.setState({tokenSelected:handelTokenClick(field,value,state,this.state)});
+    _handelTokenClick(field, value, state) {
+        this.setState({ tokenSelected: handelTokenClick(field, value, state, this.state) });
     }
 
-    _handleInputQuery(inputQuery,queryField) {
-        this.setState({searchQuery:handleInputQuery(inputQuery,queryField,this.state)});
+    _handleInputQuery(inputQuery, queryField) {
+        this.setState({ searchQuery: handleInputQuery(inputQuery, queryField, this.state) });
     }
 
     _applyFilter() {
-        let filterSubsData={}, filterState=this.state, ppsMode, _query={};
+        let filterSubsData = {}, filterState = this.state, ppsMode, _query = {};
         /**
          * for query generation
          */
         if (filterState.searchQuery["SPECIFIC_LOCATION_ZONE"]) {
-            _query.location=filterState.searchQuery["SPECIFIC_LOCATION_ZONE"]
+            _query.location = filterState.searchQuery["SPECIFIC_LOCATION_ZONE"]
         }
         if (filterState.searchQuery["BOT_ID"]) {
-            _query.butler_id=filterState.searchQuery["BOT_ID"]
+            _query.butler_id = filterState.searchQuery["BOT_ID"]
         }
-        if (filterState.tokenSelected["STATUS"] && filterState.tokenSelected["STATUS"][0] !=='any') {
-            _query.status=filterState.tokenSelected["STATUS"]
+        if (filterState.tokenSelected["STATUS"] && filterState.tokenSelected["STATUS"][0] !== 'any') {
+            _query.status = filterState.tokenSelected["STATUS"]
         }
-        if (filterState.tokenSelected["MODE"] && filterState.tokenSelected["MODE"][0] !=='any') {
-            _query.current_task=filterState.tokenSelected["MODE"]
+        if (filterState.tokenSelected["MODE"] && filterState.tokenSelected["MODE"][0] !== 'any') {
+            _query.current_task = filterState.tokenSelected["MODE"]
         }
 
-        hashHistory.push({pathname: "/system/butlerbots", query: _query})
+        hashHistory.push({ pathname: "/system/butlerbots", query: _query })
     }
 
     _clearFilter() {
         this.props.butlerfilterState({
-            tokenSelected: {"STATUS": ["any"], "MODE": ["any"],__typename:"ButlerBotsTokenSelected"},
-             searchQuery: {
-                "SPECIFIC_LOCATION_ZONE":'',
-                "BOT_ID":'',
-                 __typename:"ButlerBotsSearchQuery"
+            tokenSelected: { "STATUS": ["any"], "MODE": ["any"], __typename: "ButlerBotsTokenSelected" },
+            searchQuery: {
+                "SPECIFIC_LOCATION_ZONE": '',
+                "BOT_ID": '',
+                __typename: "ButlerBotsSearchQuery"
             },
-            defaultToken: {"STATUS": ["any"], "MODE": ["any"],__typename:"ButlerBotsDefaultToken"}
+            defaultToken: { "STATUS": ["any"], "MODE": ["any"], __typename: "ButlerBotsDefaultToken" }
         });
-        hashHistory.push({pathname: "/system/butlerbots", query: {}})
-        
+        hashHistory.push({ pathname: "/system/butlerbots", query: {} })
+
     }
 
 
-    
-    
 
-    
 
-	render(){
-    let butlerDetails=this.props.butlerDetail;
-        let butlerSearchField=this._processButlerSearchField();
-        let butlerFilterToken=this._processFilterToken();
-		return (
-			<div>
-                 <Filter>
-                <div className="gor-filter-header">
-                    <div className="gor-filter-header-h1">
-                         <FormattedMessage id="gor.filter.filterLabel" description="label for filter" defaultMessage="Filter data"/>
+
+
+
+    render() {
+        let butlerDetails = this.props.butlerDetail;
+        let butlerSearchField = this._processButlerSearchField();
+        let butlerFilterToken = this._processFilterToken();
+        return (
+            <div>
+                <Filter>
+                    <div className="gor-filter-header">
+                        <div className="gor-filter-header-h1">
+                            <FormattedMessage id="gor.filter.filterLabel" description="label for filter" defaultMessage="Filter data" />
+                        </div>
+                        <div className="gor-filter-header-h2" onClick={this._closeFilter}>
+                            <FormattedMessage id="gor.filter.hide" description="label for hide"
+                                defaultMessage="Hide" />
+                        </div>
                     </div>
-                    <div className="gor-filter-header-h2" onClick={this._closeFilter}>
-                        <FormattedMessage id="gor.filter.hide" description="label for hide" 
-                            defaultMessage="Hide"/>
+                    <div>{this.props.noResults ?
+                        <div className="gor-no-result-filter"><FormattedMessage id="gor.filter.noResult" description="label for no result"
+                            defaultMessage="No results found, please try again" /></div> : ""}
                     </div>
-                 </div>
-                    <div>{this.props.noResults?
-                            <div className="gor-no-result-filter"><FormattedMessage id="gor.filter.noResult" description="label for no result" 
-                            defaultMessage="No results found, please try again"/></div>:""}
-                    </div>
-                     <div className="gor-filter-body">
-                         <div className="gor-filter-body-input-wrap"> 
+                    <div className="gor-filter-body">
+                        <div className="gor-filter-body-input-wrap">
                             {butlerSearchField}
-                         </div>
-                         <div className="gor-filter-body-filterToken-wrap"> 
+                        </div>
+                        <div className="gor-filter-body-filterToken-wrap">
                             <div className="gor-filter-body-filterToken-section1">
                                 {butlerFilterToken.column1token}
                             </div>
-                            <div className="gor-filter-body-filterToken-section1">
+                            <div className="gor-filter-body-filterToken-section2">
                                 {butlerFilterToken.column2token}
                             </div>
-                            
-
-                         </div>
-                         
-                         
-                     </div>
-                 <div className="gor-filter-footer"> 
-                    <span className="gor-filter-footer-h2" onClick={this._clearFilter}>
-                         <FormattedMessage id="gor.filter.reset" description="label for reset" 
-                            defaultMessage="Reset"/>
-                    </span>
-                    <div className="gor-filter-btn-wrap">
-                        <button className='gor-add-btn' onClick={this._applyFilter}>
-                            {!this.props.butlerFilterSpinnerState? <FormattedMessage id="gor.filter.heading" description="filter heading"  defaultMessage="Apply filter"/> :<div className='spinnerImage'></div>}
-                        </button>
 
 
-                    </div> 
-                 </div>
+                        </div>
+
+
+                    </div>
+                    <div className="gor-filter-footer">
+                        <span className="gor-filter-footer-h2" onClick={this._clearFilter}>
+                            <FormattedMessage id="gor.filter.reset" description="label for reset"
+                                defaultMessage="Reset" />
+                        </span>
+                        <div className="gor-filter-btn-wrap">
+                            <button className='gor-add-btn' onClick={this._applyFilter}>
+                                {!this.props.butlerFilterSpinnerState ? <FormattedMessage id="gor.filter.heading" description="filter heading" defaultMessage="Apply filter" /> : <div className='spinnerImage'></div>}
+                            </button>
+
+
+                        </div>
+                    </div>
                 </Filter>
             </div>
-		);
-	}
+        );
+    }
 };
 
 
 
-ButlerBotFilter.PropTypes={
-butlerDetail: React.PropTypes.array,
-wsSubscriptionData:React.PropTypes.object,
-filterState: React.PropTypes.object,
-isFilterApplied:React.PropTypes.bool,
-showBotsFilter: React.PropTypes.func,
+ButlerBotFilter.PropTypes = {
+    butlerDetail: React.PropTypes.array,
+    wsSubscriptionData: React.PropTypes.object,
+    filterState: React.PropTypes.object,
+    isFilterApplied: React.PropTypes.bool,
+    showBotsFilter: React.PropTypes.func,
 
 };
 
-export default connect()(ButlerBotFilter) ;
+export default connect()(ButlerBotFilter);
