5060296a82f49c93110085e3df75ba9d9b8af30e
Shikher Singhal <shikher.s@greyorange.com>
2020-07-15 11:42:07 +0000

    BSS-25056

--- src/components/gor-dropdown-component/dropdown.js
+++ src/components/gor-dropdown-component/dropdown.js
@@ -1,111 +1,127 @@
-import React, { Component } from 'react';
-import List from './list';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react"
+import List from "./list"
+import ReactDOM from "react-dom"
 
 class Dropdown extends Component {
-  constructor(props){
-    super(props);
-    this._getInitialState = this._getInitialState.bind(this);
-    this._handleDocumentClick =  this._handleDocumentClick.bind(this);
-    this.state=this._getInitialState()
+  constructor(props) {
+    super(props)
+    this._getInitialState = this._getInitialState.bind(this)
+    this._handleDocumentClick = this._handleDocumentClick.bind(this)
+    this.state = this._getInitialState()
   }
 
-  _getInitialState(){
+  _getInitialState() {
     var state = {
-      dropDownVisible:false,
-      placeholder:this.props.placeholder,
-      defaultPlaceHolder:this.props.placeholder
-    };
-    return state;
-  }
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.placeholder && nextProps.placeholder !== this.state.placeholder) {
-            this.setState({
-                placeholder: nextProps.placeholder,
-                defaultPlaceHolder: nextProps.placeholder
-            })
-        }
+      dropDownVisible: false,
+      placeholder: this.props.placeholder,
+      defaultPlaceHolder: this.props.placeholder
     }
-
-  _toggleDropdown(){
-    var currentVisibility = this.state.dropDownVisible;
-    currentVisibility = !currentVisibility;
-    this.setState({dropDownVisible:currentVisibility});
+    return state
   }
 
-  _onSelect(value,isInternal){
-      if(!value){
-          return
-      }
+  _toggleDropdown() {
+    var currentVisibility = this.state.dropDownVisible
+    currentVisibility = !currentVisibility
+    this.setState({ dropDownVisible: currentVisibility })
+  }
+
+  _onSelect(value, isInternal) {
+    if (!value) return
+
     var options = this.props.options,
-    selectedOption = {},
-    matched = false,
-    len = options.length;
-    for(let i = 0 ; i < len ; i++){
-      if(value.constructor ===String && value === options[i].value){
-        selectedOption = Object.assign({},options[i]);
-        break;
-      }
-      else if(value.constructor ===Object){
-          for(let k in value){
-            if(value[k] === options[i].value[k]){
-              matched = true
-            }
-            else{
-              matched = false;
-              break
-            }
+      selectedOption = {},
+      matched = false,
+      len = options.length
+    for (let i = 0; i < len; i++) {
+      if (value.constructor === String && value === options[i].value) {
+        selectedOption = Object.assign({}, options[i])
+        break
+      } else if (value.constructor === Object) {
+        for (let k in value) {
+          if (value[k] === options[i].value[k]) {
+            matched = true
+          } else {
+            matched = false
+            break
           }
-          if(matched){
-            selectedOption = Object.assign({},options[i]);
-            break;
-          }
+        }
+        if (matched) {
+          selectedOption = Object.assign({}, options[i])
+          break
+        }
       }
     }
-    if(!selectedOption.value){
-      selectedOption = Object.assign({},options[0]);
+    if (!selectedOption.value) {
+      selectedOption = Object.assign({}, options[0])
     }
-    this.setState({
-      dropDownVisible:!isInternal ? !this.state.dropDownVisible : this.state.dropDownVisible,
-      placeholder:selectedOption.label || this.state.defaultPlaceHolder
-    },function(){
-        if(selectedOption.value){
-          this.props.onSelectHandler(selectedOption);
+    this.setState(
+      {
+        dropDownVisible: !isInternal
+          ? !this.state.dropDownVisible
+          : this.state.dropDownVisible,
+        placeholder: selectedOption.label || this.state.defaultPlaceHolder
+      },
+      function() {
+        if (selectedOption.value) {
+          this.props.onSelectHandler(selectedOption)
+        }
       }
-    })
-    
+    )
   }
 
   _handleDocumentClick() {
-     if (!ReactDOM.findDOMNode(this).contains(event.target)) {
-       this.setState({dropDownVisible: false});
-     }
- }
+    if (!ReactDOM.findDOMNode(this).contains(event.target)) {
+      this.setState({ dropDownVisible: false })
+    }
+  }
 
-
-  componentDidMount(){
-      document.addEventListener('click',this._handleDocumentClick,false);
+  componentDidMount() {
+    document.addEventListener("click", this._handleDocumentClick, false)
   }
 
   componentWillUnmount() {
-      document.removeEventListener("click", this._handleDocumentClick,false)
+    document.removeEventListener("click", this._handleDocumentClick, false)
   }
-
 
   render() {
-    
     return (
-          <div className="gor-dropdown-wrapper" onClick={!this.props.disabled ? this._toggleDropdown.bind(this) : null}>
-            <span className={this.props.disabled?'gor-dropdown':'gor-dropdown gor-enabled'}   >{!this.props.resetOnSelect ? this.state.placeholder : this.state.defaultPlaceHolder}</span>
-            <span className={this.state.dropDownVisible ? "gor-dropdown-arrow up" : "gor-dropdown-arrow"}></span>
-            <List data={this.props.options} selectedOption={this.props.selectedOption} optionAction={this._onSelect.bind(this)}
-              dropDownVisible={this.state.dropDownVisible} />
-          </div>
-    );
+      <div
+        className="gor-dropdown-wrapper"
+        onClick={!this.props.disabled ? this._toggleDropdown.bind(this) : null}
+      >
+        <span
+          className={
+            this.props.disabled ? "gor-dropdown" : "gor-dropdown gor-enabled"
+          }
+        >
+          <span
+            className={
+              this.props.placeholderClass
+                ? "gor-dropdown-placeholder-padding"
+                : ""
+            }
+          >
+            {!this.props.resetOnSelect
+              ? this.state.placeholder
+              : this.state.defaultPlaceHolder}
+          </span>
+        </span>
+        <span
+          className={
+            this.state.dropDownVisible
+              ? "gor-dropdown-arrow up"
+              : "gor-dropdown-arrow"
+          }
+        ></span>
+        <List
+          data={this.props.options}
+          selectedOption={this.props.selectedOption}
+          optionAction={this._onSelect.bind(this)}
+          dropDownVisible={this.state.dropDownVisible}
+        />
+      </div>
+    )
   }
-
-
 }
 
-export default Dropdown;
+export default Dropdown
